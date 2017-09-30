1def76e2cd418ad0adbc3e109b9eb2822ae8f14f
Bjverde <bjverde@yahoo.com.br>
2017-09-30 02:58:13 -0300

    arquivos em UTF-8

--- base/exemplos/menu_principal.php
+++ base/exemplos/menu_principal.php
@@ -2,7 +2,7 @@
 
 /*
  * Formdin Framework
- * Copyright (C) 2012 Ministério do Planejamento
+ * Copyright (C) 2012 MinistÃ©rio do Planejamento
  * ----------------------------------------------------------------------------
  * This file is part of Formdin Framework.
  *
@@ -20,20 +20,20 @@
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
- * Este arquivo é parte do Framework Formdin.
+ * Este arquivo Ã© parte do Framework Formdin.
  *
- * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
- * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * O Framework Formdin Ã© um software livre; vocÃª pode redistribuÃ­-lo e/ou
+ * modificÃ¡-lo dentro dos termos da GNU LGPL versÃ£o 3 como publicada pela FundaÃ§Ã£o
  * do Software Livre (FSF).
  *
- * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
- * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
- * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * Este programa Ã© distribuÃ­do na esperanÃ§a que possa ser Ãºtil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implÃ­cita de ADEQUAÃ‡ÃƒO a qualquer MERCADO ou
+ * APLICAÃ‡ÃƒO EM PARTICULAR. Veja a LicenÃ§a PÃºblica Geral GNU/LGPL em portuguÃªs
  * para maiores detalhes.
  *
- * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
- * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
- * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * VocÃª deve ter recebido uma cÃ³pia da GNU LGPL versÃ£o 3, sob o tÃ­tulo
+ * "LICENCA.txt", junto com esse programa. Se nÃ£o, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a FundaÃ§Ã£o do Software Livre (FSF) Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
 
@@ -41,13 +41,13 @@
 // url para teste: http://index.php?modulo=menu_principal.php&ajax=1&content-type=xml
 $menu =  new TMenuDhtmlx();
 $menu->add('1',null,'Campos',null,null,'user916.gif');
-$menu->add('11','1','Campo Texto',null,'Declaração de texto' );
+$menu->add('11','1','Campo Texto',null,'DeclaraÃ§Ã£o de texto' );
 $menu->add('11.1','11','Campo Texto','exe_TextField.php')->setJsonParams("{'p1':'parametro_1','p2':'parametro_2'}");
 
 $menu->add('11.2','11','Autocompletar','exe_autocomplete.php');
 $menu->add('11.3','11','Autocompletar II','exe_autocomplete2.php');
 $menu->add('11.4','11','Consulta On-line I','exe_onlinesearch1.php');
-$menu->add('11.5','11','Entrada com Máscara','exe_maskField.php');
+$menu->add('11.5','11','Entrada com MÃ¡scara','exe_maskField.php');
 
 
 $menu->add('12','1','Campo HTML','exe_HtmlField.php');
@@ -58,14 +58,14 @@ $menu->add('16','1','Campo Check','exe_CheckField.php');
 $menu->add('17','1','Campo Arquivo');
   $menu->add('171','17','Assincrono','exe_FileAsync.php');
   $menu->add('172','17','Normal','exe_TFile.php');
-$menu->add('18','1','Campo Numérico','exe_NumberField.php');
+$menu->add('18','1','Campo NumÃ©rico','exe_NumberField.php');
 $menu->add('19','1','Campo CEP'	,'exe_CepField.php');
 $menu->add('20','1','Campo Telefone'	,'exe_FoneField.php');
 $menu->add('21','1','Campo Cpf/Cnpj'	,'exe_campo_cpf_cnpj.php');
 
 $menu->add('110','1','Campo Data'	,'exe_DateField.php');
 $menu->add('111','1','Campo Select Diretorio/Pasta'	,'exe_OpenDirField.php');
-$menu->add('112','1','Campo Fuso Horário'	,'exe_TTimeZoneField.php');
+$menu->add('112','1','Campo Fuso HorÃ¡rio'	,'exe_TTimeZoneField.php');
 $menu->add('113','1','Campo Editor'	,'exe_TTextEditor.php');
 $menu->add('114','1','Campo Memo'	,'exe_TMemo.php');
 $menu->add('115','1','Campo Senha'	,'exe_TPasswordField.php');
@@ -87,20 +87,20 @@ $menu->add('22','2','Grupo');
 
 $menu->add('23','2','Aba'		,'exe_aba_1.php');
 $menu->add('24','2','TreeView');
-$menu->add('241','24','Dentro do Formulário' ,'exe_tree_view_1.php');
-$menu->add('242','24','Fora do Formulário'	,'exe_tree_view_2.php');
+$menu->add('241','24','Dentro do FormulÃ¡rio' ,'exe_tree_view_1.php');
+$menu->add('242','24','Fora do FormulÃ¡rio'	,'exe_tree_view_2.php');
 $menu->add('243','24','User Data - Array'	,'exe_tree_view_3.php');
-$menu->add('244','24','Uf x Municípios'		,'exe_tree_view_4.php');
-$menu->add('245','24','Uf x Municípios com SetXmlFile()' ,'exe_tree_view_5.php');
+$menu->add('244','24','Uf x MunicÃ­pios'		,'exe_tree_view_4.php');
+$menu->add('245','24','Uf x MunicÃ­pios com SetXmlFile()' ,'exe_tree_view_5.php');
 
-$menu->add('4',null,'Ajuda On-line (sqlite)','exe_documentacao_online.inc','Confeção do texto de ajuda gravando no banco de dados sqlite');
+$menu->add('4',null,'Ajuda On-line (sqlite)','exe_documentacao_online.inc','ConfeÃ§Ã£o do texto de ajuda gravando no banco de dados sqlite');
 
 
 $menu->add('5',null,'Ajax');
 $menu->add('51','5','Exemplo 1','exe_ajax_1.php');
 $menu->add('52','5','Atualizar Campos','exe_ajax_2.php');
-$menu->add('53','5','Botão Ajax','exe_TButtonAjax.php');
-$menu->add('54','5','Ajax com Semáforo','exe_ajax_semaphore.php');
+$menu->add('53','5','BotÃ£o Ajax','exe_TButtonAjax.php');
+$menu->add('54','5','Ajax com SemÃ¡foro','exe_ajax_semaphore.php');
 
 $menu->add('6',null,'PDF');
 $menu->add('61','6','Exemplo 1','exe_pdf_1.php');
@@ -108,15 +108,15 @@ $menu->add('62','6','Exemplo 2','exe_pdf_2.php');
 
 $menu->add('7',null,'Mensagens');
 $menu->add('71','7','Exemplo 1','exe_mensagem.php');
-$menu->add('72','7','Caixa de Confirmação','exe_confirmDialog.php');
+$menu->add('72','7','Caixa de ConfirmaÃ§Ã£o','exe_confirmDialog.php');
 
 $menu->add('8',null,'Gride');
 $menu->add('81','8','Exemplo 1','exe_gride_1.php');
 $menu->add('82','8','Exemplo 2 - Anexos - Ajax','exe_gride_2.php');
 $menu->add('83','8','Grid Offline','exe_gride_3.php');
 $menu->add('84','8','Grid Offline com fwGetGrid()','exe_gride_4.php');
-$menu->add('85','8','Paginação','exe_gride_paginacao.php');
-$menu->add('86','8','Função getGrid()','exe_gride_4.php');
+$menu->add('85','8','PaginaÃ§Ã£o','exe_gride_paginacao.php');
+$menu->add('86','8','FunÃ§Ã£o getGrid()','exe_gride_4.php');
 $menu->add('87','8','Gride com Campos 1','exe_gride_10.php');
 $menu->add('88','8','Gride com Campos 2','exe_gride_11.php');
 
@@ -126,16 +126,16 @@ $menu->add('92','9','Exemplo Sqlite e Mysql','exe_pdo_2.php');
 $menu->add('93','9','Exemplo Postgres');
  $menu->add('931','93','DAO e VO','exe_pg_dao_vo_1.php');
 $menu->add('94','9','Exemplo Firebird');
- $menu->add('941','94','Conexão','exe_firebird_1.php');
+ $menu->add('941','94','ConexÃ£o','exe_firebird_1.php');
 $menu->add('99','9','Gerador VO/DAO','../includes/gerador_vo_dao.php');
-$menu->add('95','9','Testar Conexão','exe_teste_conexao.php');
+$menu->add('95','9','Testar ConexÃ£o','exe_teste_conexao.php');
 
 
 $menu->add('101',null,'Hints');
 $menu->add('1011','101','Exemplo I','exe_hint.php');
 
 $menu->add('102',null,'Teste','teste.php');
-$menu->add('103',null,'Formulário');
+$menu->add('103',null,'FormulÃ¡rio');
 	$menu->add('1031','103','Normal','exe_TForm.php');
 	$menu->add('1032','103','Subcadastro','exe_TForm2.php');
 	$menu->add('1033','103','Boxes','exe_TBox.php');
