8fb794dac3fa931d68a70a32ce24a60795ef0ff5
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2017-10-02 19:20:57 -0300

    correcao do menu

--- examples/includes/menu.php
+++ examples/includes/menu.php
@@ -123,9 +123,9 @@ $menu->add('93','9','Exemplo Postgres');
  $menu->add('931','93','DAO e VO','exe_pg_dao_vo_1.php');
 $menu->add('94','9','Exemplo Firebird');
  $menu->add('941','94','Conexão','exe_firebird_1.php');
-$menu->add('99','9','Gerador VO/DAO','../base/includes/gerador_vo_dao.php');
-$menu->add('95','9','Testar Conexão','exe_teste_conexao.php');
-$menu->add('95','9','Dados de Apoio','cad_apoio_pdo.php');
+$menu->add('95','9','Gerador VO/DAO','../base/includes/gerador_vo_dao.php');
+$menu->add('96','9','Testar Conexão','exe_teste_conexao.php');
+$menu->add('97','9','Dados de Apoio','cad_apoio_pdo.php');
 
 
 $menu->add('101',null,'Hints');
