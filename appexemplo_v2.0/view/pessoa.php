d3bfc2e9b9811f263734b1f74e6cb174017a72d9
Bjverde <bjverde@yahoo.com.br>
2017-10-02 04:19:08 -0300

    tela semi funcional

--- appexemplo_v2.0/view/pessoa.php
+++ appexemplo_v2.0/view/pessoa.php
@@ -1,26 +1,52 @@
 <?php
 
-$dados = pessoaDAO::selectAll('nome');
-var_dump($dados);
+d($_POST);
 
-$gride = new TGrid( 'gdPessoa' // id do gride
+
+$frm = new TForm('Cadastro de Pessoa');
+
+$acao = isset($acao) ? $acao : null;
+switch( $acao ) {
+    case 'Salvar':
+        $vo = new PessoaVO();
+        $frm->setVo( $vo );
+        pessoaDAO::insert( $vo );
+    //------------------------------------------------------------------
+    case 'Limpar':
+        $frm->clearFields();
+        break;
+    //--------------------------------------------------------------------
+    case 'gd_excluir':
+        $id = $frm->get( 'IDPESSOA' ) ;
+        pessoaDAO::delete( $id );
+    break;
+	//--------------------------------------------------------------------
+}
+
+$dados = pessoaDAO::selectAll('nome');
+$gride = new TGrid( 'gd' // id do gride
                    ,'Lista de Pessoas' // titulo do gride
                    ,$dados 	    // array de dados
                    ,null		// altura do gride
                    ,null		// largura do gride
                    ,'IDPESSOA'  // chave primaria
+                   ,'IDPESSOA|IDPESSOA,NOME|NOME,TIPO|TIPO,DAT_INCLUSAO|DAT_INCLUSAO'  // update dos campos
                 );
 
-$gride->addColumn('IDPESSOA','id',100,'left');
+$gride->addColumn('IDPESSOA','id',50,'center');
 $gride->addColumn('NOME','Nome Pessoa',100,'left');
-$gride->addColumn('TIPO','Tipo',100,'left');
-$gride->addColumn('DAT_INCLUSAO','Data Inclusão',100,'left');
+$gride->addColumn('TIPO','Tipo',50,'center');
+$gride->addColumn('DAT_INCLUSAO','Data Inclusão',100,'center');
 
 
 
-$frm = new TForm('Cadastro de Pessoa');
+
 $frm->addHiddenField( 'IDPESSOA' ); // coluna chave da tabela
+$frm->addTextField('NOME', 'Nome:',50,false);
+$frm->addSelectField('TIPO'	, 'Tipo Pessoa:',null,'PF=Pessoa física,PJ=Pessoa jurídica',false);
+$frm->addTextField('DAT_INCLUSAO', 'Data:',50,false);
+//$frm->addDateField('DAT_INCLUSAO','Data:',null);
 $frm->addHtmlField('gride',$gride);
-$frm->setAction('Limpar');
+$frm->setAction( 'Salvar,Limpar' );
 $frm->show();
 ?>
