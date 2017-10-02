a2d5c0132fe0315815e26e668aec3ea8a8086476
Bjverde <bjverde@yahoo.com.br>
2017-10-01 21:40:19 -0300

    arquivo de banco de dados

---
+++ appexemplo_v2.0/includes/con_default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Exemplo para MySQL como banco default
+* para outros bancos veja o arquivo TDAO.class.php
+*/
+$dbType = "mysql";
+$host = "localhost";
+$port = "3306";
+$database = "form_exemplo";
+$username = "form_exemplo";
+$password = "123456";
+$utf8=1;
+
+?>
