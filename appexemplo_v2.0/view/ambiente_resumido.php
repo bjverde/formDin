3ae6ee08c77570857f11b577eac4d88e445db1a5
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2017-10-02 20:20:29 -0300

    Alteração para a nova versão do drive SQL Server

--- appexemplo_v2.0/view/ambiente_resumido.php
+++ appexemplo_v2.0/view/ambiente_resumido.php
@@ -29,6 +29,24 @@ function testar($extensao=null,$html){
 	}
 }
 
+function infoSQLServer(){
+	if(PHP_OS == "Linux"){
+		$msg = "Para utilizar Linux com Microsoft SQL Server será utilizado o PDO com o drive dblib";
+	}else{
+		switch(PHP_INT_SIZE) {
+			case 4:
+				$msg = 'Para utilizar Windows com Microsoft SQL Server utilize o PDO com o drive sqlsrv';
+				break;
+			case 8:
+				$msg = '<span class="vermelho">ATENÇÃO o Drive da Microsoft não funciona em Windows 64 bits</span>, instale o drive não oficial';
+				break;
+			default:
+				$msg  = 'PHP_INT_SIZE is ' . PHP_INT_SIZE;
+		}
+	}
+	return $msg;
+}
+
 function phpVersionOK(){
 	$texto = '<b>Versão do PHP</b>: ';
 	if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
@@ -38,10 +56,8 @@ function phpVersionOK(){
 	}
 	return $texto;
 }
-
-
 	
-	$frm = new TForm('Configurações do PHP',null,600);
+	$frm = new TForm('Configurações do PHP',null,800,600);
 	$html = $frm->addHtmlField('conf','');
 	$html->setCss('font-size','14px');
 
@@ -61,12 +77,12 @@ function phpVersionOK(){
 	testar('mysql',$html);
 	testar('interbase',$html);
 	testar('oci8',$html);
-	testar('mssql',$html);
+	testar('sqlsrv',$html);
 
 	$html->add('<br>');
 	testar('pdo',$html);
 	testar('PDO_Firebird',$html);
-	testar('pdo_mssql',$html);
+	$html->add(infoSQLServer()); testar('pdo_sqlsrv',$html);
 	testar('pdo_mysql',$html);
 	testar('PDO_OCI',$html);
 	testar('PDO_ODBC',$html);
