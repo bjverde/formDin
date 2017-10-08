70c3aae4721a108ed1708a4b510028a000ed9c87
Bjverde <bjverde@yahoo.com.br>
2017-10-08 17:50:46 -0300

    inclusão da informação do usuario

--- appexemplo_v2.5/services/loginService.php
+++ appexemplo_v2.5/services/loginService.php
@@ -6,6 +6,8 @@ class loginService {
 	public static function validarLogin( $nom_user, $pwd_user )	{
         $dados = Acesso_userDAO::selectUser($nom_user, $pwd_user);
         if ( count($dados['IDUSER']) == 1 ) {
+            $_SESSION[APLICATIVO]['IDUSER'] = $dados['IDUSER'][0];
+            $_SESSION[APLICATIVO]['LOGIN']  = $dados['NOM_USER'][0];
             $msg = 1;
         }else{
             $msg = 'Login Invalido !';
