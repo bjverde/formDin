08f20e4120dcaba5986538ea87a6a33aa1fac339
Bjverde <bjverde@yahoo.com.br>
2017-10-11 11:07:21 -0300

    correção de caracteres

--- appexemplo_v1.0/includes/menu.php
+++ appexemplo_v1.0/includes/menu.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Formdin Framework
- * Copyright (C) 2012 Minist?rio do Planejamento
+ * Copyright (C) 2012 Ministério do Planejamento
  * ----------------------------------------------------------------------------
  * This file is part of Formdin Framework.
  *
@@ -19,15 +19,15 @@
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
- * Este arquivo ? parte do Framework Formdin.
+ * Este arquivo é parte do Framework Formdin.
  *
- * O Framework Formdin ? um software livre; voc? pode redistribu?-lo e/ou
- * modific?-lo dentro dos termos da GNU LGPL vers?o 3 como publicada pela Funda??o
+ * O Framework Formdin é um software livre; você pode redistribu?-lo e/ou
+ * modific?-lo dentro dos termos da GNU LGPL vers?o 3 como publicada pela Fundação
  * do Software Livre (FSF).
  *
- * Este programa ? distribu?do na esperan?a que possa ser ?til, mas SEM NENHUMA
+ * Este programa é distribuédo na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia impl?cita de ADEQUA??O a qualquer MERCADO ou
- * APLICA??O EM PARTICULAR. Veja a Licen?a P?blica Geral GNU/LGPL em portugu?s
+ * APLICAÇÃO EM PARTICULAR. Veja a Licen?a Pública Geral GNU/LGPL em portugu?s
  * para maiores detalhes.
  *
  * Voc? deve ter recebido uma c?pia da GNU LGPL vers?o 3, sob o t?tulo
@@ -40,12 +40,12 @@
 // url para teste: http://index.php?modulo=menu_principal.php&ajax=1&content-type=xml
 $menu =  new TMenuDhtmlx();
 $menu->add('1',null,'Campos',null,null,'user916.gif');
-	$menu->add('11','1','Campo Texto',null,'Declara??o de texto' );
+	$menu->add('11','1','Campo Texto',null,'Declaração de texto' );
 		$menu->add('11.1','11','Campo Texto','view/fields/exe_TextField.php')->setJsonParams("{'p1':'parametro_1','p2':'parametro_2'}");
 		$menu->add('11.2','11','Autocompletar','view/fields/exe_autocomplete.php');
 		$menu->add('11.3','11','Autocompletar II','view/fields/exe_autocomplete2.php');
 		$menu->add('11.4','11','Consulta On-line I','view/fields/exe_onlinesearch1.php');
-		$menu->add('11.5','11','Entrada com M?scara','view/fields/exe_maskField.php');
+		$menu->add('11.5','11','Entrada com Máscara','view/fields/exe_maskField.php');
 	$menu->add('12','1','Campo HTML','view/fields/exe_HtmlField.php');
 	$menu->add('13','1','Campo Coord GMS','view/fields/exe_CoordGmsField.php');
 	$menu->add('14','1','Campo Select','view/fields/exe_SelectField.php');
@@ -55,13 +55,13 @@ $menu->add('1',null,'Campos',null,null,'user916.gif');
 		$menu->add('171','17','Assincrono','view/fields/exe_FileAsync.php');
 		$menu->add('172','17','Normal','view/fields/exe_TFile.php');
 		$menu->add('173','17','TAssincrono','view/fields/exe_TFileAsync.php');
-	$menu->add('18','1','Campo Num?rico','view/fields/exe_NumberField.php');
+	$menu->add('18','1','Campo Numúrico','view/fields/exe_NumberField.php');
 	$menu->add('19','1','Campo CEP'	,'view/fields/exe_CepField.php');
 	$menu->add('20','1','Campo Telefone'	,'view/fields/exe_FoneField.php');
 	$menu->add('21','1','Campo Cpf/Cnpj'	,'view/fields/exe_campo_cpf_cnpj.php');
 	$menu->add('110','1','Campo Data'	,'view/fields/exe_DateField.php');
 	$menu->add('111','1','Campo Select Diretorio/Pasta'	,'view/fields/exe_OpenDirField.php');
-	$menu->add('112','1','Campo Fuso Hor?rio - ERRO'	,'exe_TTimeZoneField.php');
+	$menu->add('112','1','Campo Fuso Horário - ERRO'	,'exe_TTimeZoneField.php');
 	$menu->add('113','1','Campo Editor'	,'view/fields/exe_TTextEditor.php');
 	$menu->add('114','1','Campo Memo - ERRO'	,'exe_TMemo.php');
 	$menu->add('115','1','Campo Senha'	,'view/fields/exe_TPasswordField.php');
@@ -83,11 +83,11 @@ $menu->add('22','2','Grupo');
 
 $menu->add('23','2','Aba'		,'exe_aba_1.php');
 $menu->add('24','2','TreeView');
-$menu->add('241','24','Dentro do Formul?rio' ,'exe_tree_view_1.php');
-$menu->add('242','24','Fora do Formul?rio'	,'exe_tree_view_2.php');
+$menu->add('241','24','Dentro do Formulário' ,'exe_tree_view_1.php');
+$menu->add('242','24','Fora do Formulário'	,'exe_tree_view_2.php');
 $menu->add('243','24','User Data - Array'	,'exe_tree_view_3.php');
-$menu->add('244','24','Uf x Munic?pios'		,'exe_tree_view_4.php');
-$menu->add('245','24','Uf x Munic?pios com SetXmlFile()' ,'exe_tree_view_5.php');
+$menu->add('244','24','Uf x Municípios'		,'exe_tree_view_4.php');
+$menu->add('245','24','Uf x Municípios com SetXmlFile()' ,'exe_tree_view_5.php');
 
 $menu->add('4',null,'Ajuda On-line (sqlite)','exe_documentacao_online.inc','Confe??o do texto de ajuda gravando no banco de dados sqlite');
 
@@ -112,8 +112,8 @@ $menu->add('81','8','Exemplo 1','exe_gride_1.php');
 $menu->add('82','8','Exemplo 2 - Anexos - Ajax','exe_gride_2.php');
 $menu->add('83','8','Grid Offline','exe_gride_3.php');
 $menu->add('84','8','Grid Offline com fwGetGrid()','exe_gride_4.php');
-$menu->add('85','8','Pagina??o','exe_gride_paginacao.php');
-$menu->add('86','8','Fun??o getGrid()','exe_gride_4.php');
+$menu->add('85','8','Paginação','exe_gride_paginacao.php');
+$menu->add('86','8','Função getGrid()','exe_gride_4.php');
 $menu->add('87','8','Gride com Campos 1','exe_gride_10.php');
 $menu->add('88','8','Gride com Campos 2','exe_gride_11.php');
 
@@ -123,7 +123,7 @@ $menu->add('92','9','Exemplo Sqlite e Mysql','exe_pdo_2.php');
 $menu->add('93','9','Exemplo Postgres');
  $menu->add('931','93','DAO e VO','exe_pg_dao_vo_1.php');
 $menu->add('94','9','Exemplo Firebird');
- $menu->add('941','94','Conex?o','exe_firebird_1.php');
+ $menu->add('941','94','Conexão','exe_firebird_1.php');
 $menu->add('95','9','Gerador VO/DAO','../base/includes/gerador_vo_dao.php');
 $menu->add('96','9','Testar Conex?o','exe_teste_conexao.php');
 $menu->add('97','9','Dados de Apoio','cad_apoio_pdo.php');
