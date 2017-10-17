bf5a52b0240bea83a2b118f33a0f2a64a5dcd243
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2017-10-17 18:47:40 -0200

    correção : verifica se a constante já foi definida

--- base/classes/webform/TApplication.class.php
+++ base/classes/webform/TApplication.class.php
@@ -38,7 +38,7 @@
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
-define('DS', DIRECTORY_SEPARATOR);
+if(!defined('DS')){ define('DS',DIRECTORY_SEPARATOR); }
 $currentl_dir = dirname(__FILE__);
 $helps_dir = $currentl_dir.DS.'..'.DS.'helpers'.DS;
 
