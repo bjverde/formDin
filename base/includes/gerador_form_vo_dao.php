0dc28f54ef06eea7a6e85b5e40abe3e6434ed5cd
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2017-10-19 15:01:35 -0200

    correção do schema

--- base/includes/gerador_form_vo_dao.php
+++ base/includes/gerador_form_vo_dao.php
@@ -28,9 +28,9 @@
  * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
  * do Software Livre (FSF).
  *
- * Este programa é distribuí1do na esperança que possa ser útil, mas SEM NENHUMA
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
- * APLICAÇÃO EM PARTICULAR. Veja a Licen?a Pública Geral GNU/LGPL em portugu?s
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
  * para maiores detalhes.
  *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
@@ -39,87 +39,87 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
 
- function verifyPath($path){
-	$diretorio = str_replace('//','/',$path.'/');
-	@mkdir($diretorio,"775",true);
-	if( ! file_exists( $diretorio ) ) {
-		$diretorio = 0;
-	}
-	return $diretorio;
+function verifyPath($path){
+    $diretorio = str_replace('//','/',$path.'/');
+    @mkdir($diretorio,"775",true);
+    if( ! file_exists( $diretorio ) ) {
+        $diretorio = 0;
+    }
+    return $diretorio;
 }
 
 
 $frm = new TForm('Gerador de From, VO e DAO',650,700);
 $frm->setFlat(true);
 $frm->addGroupField('gpx1','Informações do form');
-	$frm->addTextField('form_title','Título do formulario:',50,true,50);
-	$frm->addTextField('form_dir','Diretório do formulário:',50,true,150,'modulos/');
-	$frm->addTextField('form_name','Nome do arquivo formulario:',50,true,50);
+$frm->addTextField('form_title','Título do formulario:',50,true,50);
+$frm->addTextField('form_dir','Diretório do formulário:',50,true,150,'modulos/');
+$frm->addTextField('form_name','Nome do arquivo formulario:',50,true,50);
 $frm->closeGroup();
 $frm->addGroupField('gpxE','Informações do Esquema');
 $frm->addHtmlField('html','Em alguns bancos como o MS SQL Server a informação do esquema é nescessaria para as intruções de banco. SE Marcar a opção SIM nas DAOs será incluido a constante SCHEME antes do nome da tabela.',null,null,50)->setCss('border','1px solid blue');
-$frm->addSelectField('sit_const_scheme','Constante Esquema:',null,'0=Não,1=Sim',null,null,'0');
+$frm->addSelectField('sit_const_schema','Constante Esquema:',null,'0=Não,1=Sim',null,null,'0');
 $frm->closeGroup();
 $frm->addGroupField('gpx2','Informações do VO e DAO');
-	$frm->addTextField('diretorio','Diretório:',50,true);
-	$frm->addTextField('tabela','Nome da Tabela:',30,true);
-	$frm->addTextField('coluna_chave','Nome da Coluna Chave:',30);
-	$frm->addMemoField('colunas','Colunas:',5000,true,45,20);
-$frm->closeGroup();	
+$frm->addTextField('diretorio','Diretório:',50,true);
+$frm->addTextField('tabela','Nome da Tabela:',30,true);
+$frm->addTextField('coluna_chave','Nome da Coluna Chave:',30);
+$frm->addMemoField('colunas','Colunas:',5000,true,45,20);
+$frm->closeGroup();
 
 $frm->setAction('Gerar');
 
 if(!$frm->get('diretorio')) {
-	$frm->set('diretorio','dao/');
+    $frm->set('diretorio','dao/');
 }
 
 $acao = ( isset($acao) ) ? $acao : '';
 switch( $acao ) {
-	case 'Gerar':
-		if( $frm->validate() ) {
-			$diretorio = str_replace('//','/',$frm->get('diretorio').'/');
-			@mkdir($diretorio,"775",true);
-			if( ! file_exists( $diretorio ) ) {
-				$frm->setMessage('Diretório '.$diretorio.' não existe!');
-				break;
-			}
-			$txt = preg_replace('/'.chr(13).'/',',',$frm->get('colunas'));
-			$txt = preg_replace('/'.chr(10).'/',',',$txt);
-			$txt = preg_replace('/\,\,/',',',$txt);
-			$txt = preg_replace('/ /','',$txt);
-			$txt = preg_replace('/\,\,/',',',$txt);
-			if( substr($txt,-1) ==',') {
-				$txt=substr($txt,0,strlen($txt)-1);
-			}
-			$listColumns = explode(',',$txt);
-			$coluna_chave = $frm->get('coluna_chave') ? $frm->get('coluna_chave') : $listColumns[0];			
-			$gerador = new TDAOCreate($frm->get('tabela'), $coluna_chave, $diretorio);
-			foreach($listColumns as $k=>$v) {
-				$gerador->addColumn($v);
-			}
-			$showScheme = $frm->get('sit_const_scheme');
-			$gerador->setShowScheme($showScheme);
-			$gerador->saveVO();
-			$gerador->saveDAO();
-
-			$diretorio = str_replace('//','/',$frm->get('form_dir').'/');
-			@mkdir($diretorio,"775",true);
-			if( ! file_exists( $diretorio ) ) {
-				$frm->setMessage('Diretório '.$diretorio.' não existe!');
-				break;
-			}
-			
-			$geradorForm = new TFormCreate();
-			$geradorForm ->setFormTitle( $frm->get('form_title') );
-			$geradorForm ->setFormPath( $diretorio );
-			$geradorForm ->setFormFileName($frm->get('form_name'));
-			$geradorForm ->setPrimaryKeyTable($frm->get('coluna_chave'));
-			$geradorForm ->setTableRef($frm->get('tabela'));
-			$geradorForm ->setListColunnsName($listColumns);
-			$geradorForm ->saveForm();
-			$frm->setMessage('Fim');
-		}
-	break;
+    case 'Gerar':
+        if( $frm->validate() ) {
+            $diretorio = str_replace('//','/',$frm->get('diretorio').'/');
+            @mkdir($diretorio,"775",true);
+            if( ! file_exists( $diretorio ) ) {
+                $frm->setMessage('Diretório '.$diretorio.' não existe!');
+                break;
+            }
+            $txt = preg_replace('/'.chr(13).'/',',',$frm->get('colunas'));
+            $txt = preg_replace('/'.chr(10).'/',',',$txt);
+            $txt = preg_replace('/\,\,/',',',$txt);
+            $txt = preg_replace('/ /','',$txt);
+            $txt = preg_replace('/\,\,/',',',$txt);
+            if( substr($txt,-1) ==',') {
+                $txt=substr($txt,0,strlen($txt)-1);
+            }
+            $listColumns = explode(',',$txt);
+            $coluna_chave = $frm->get('coluna_chave') ? $frm->get('coluna_chave') : $listColumns[0];
+            $gerador = new TDAOCreate($frm->get('tabela'), $coluna_chave, $diretorio);
+            foreach($listColumns as $k=>$v) {
+                $gerador->addColumn($v);
+            }
+            $showSchema = $frm->get('sit_const_schema');
+            $gerador->setShowSchema($showSchema);
+            $gerador->saveVO();
+            $gerador->saveDAO();
+            
+            $diretorio = str_replace('//','/',$frm->get('form_dir').'/');
+            @mkdir($diretorio,"775",true);
+            if( ! file_exists( $diretorio ) ) {
+                $frm->setMessage('Diretório '.$diretorio.' não existe!');
+                break;
+            }
+            
+            $geradorForm = new TFormCreate();
+            $geradorForm ->setFormTitle( $frm->get('form_title') );
+            $geradorForm ->setFormPath( $diretorio );
+            $geradorForm ->setFormFileName($frm->get('form_name'));
+            $geradorForm ->setPrimaryKeyTable($frm->get('coluna_chave'));
+            $geradorForm ->setTableRef($frm->get('tabela'));
+            $geradorForm ->setListColunnsName($listColumns);
+            $geradorForm ->saveForm();
+            $frm->setMessage('Fim');
+        }
+        break;
 }
 
 $frm->show();
