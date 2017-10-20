bfeea5957607604bdebf6ca439c6131f2eb06833
Bjverde <bjverde@yahoo.com.br>
2017-10-20 02:16:48 -0200

    inclusão do editor no exemplo

--- appexemplo_v1.0/includes/menu.php
+++ appexemplo_v1.0/includes/menu.php
@@ -164,6 +164,7 @@ $menu->add('104',null,'TZip','exe_TZip.php');
 $menu->add('199',null,'Temas do Menu','exe_menu_tema.php');
 
 $menu->add(-20,0,'Exemplos',null,'Exemplo de Recursos do Formulário Diâmico IV','acessibilidade-brasil.gif');
+$menu->add(-26020,-20,'editor','exe_editor_html.inc');
 $menu->add(-26001,-20,'Criação de Gride','exe_gride.inc');
 $menu->add(-26002,-20,'Grid Off-Line - Formdin3','exe_gride_off_line.inc');
 $menu->add(-26003,-20,'Grid Off-Line - FormDin4','exe_gride_off_line_novo.inc');
