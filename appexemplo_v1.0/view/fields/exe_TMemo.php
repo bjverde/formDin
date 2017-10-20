436c2e7912570c050a872565f6c56da7713d4d71
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2017-10-20 17:29:31 -0200

    licença e Undefined variable

--- appexemplo_v1.0/view/fields/exe_TMemo.php
+++ appexemplo_v1.0/view/fields/exe_TMemo.php
@@ -1,36 +1,38 @@
 <?php
-
 /*
  * Formdin Framework
  * Copyright (C) 2012 Ministério do Planejamento
+ * Criado por Luís Eugênio Barbosa
+ * Essa versão é um Fork https://github.com/bjverde/formDin
+ *
  * ----------------------------------------------------------------------------
  * This file is part of Formdin Framework.
- * 
+ *
  * Formdin Framework is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License version 3
  * along with this program; if not,  see <http://www.gnu.org/licenses/>
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
  * Este arquivo é parte do Framework Formdin.
- * 
+ *
  * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
  * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
  * do Software Livre (FSF).
- * 
+ *
  * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
- * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em portugu?s
  * para maiores detalhes.
- * 
+ *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
  * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
@@ -47,22 +49,21 @@ $frm->addMemoField('campo_memo_2','Documento:',100000,false,80,5,null,null,false
 $frm->addJavascript('fwSetHtmlEditorPreview("campo_memo_2")');
 $frm->setAction('Validar');
 $frm->addButton('Preview',null,'btnPreview','doPreview()');
-if( $acao =='Validar')
-{
-	$frm->validate();
+
+$acao = isset($acao) ? $acao : null;
+if ($acao == 'Validar') {
+    $frm->validate();
 }
 //d($acao);
 $frm->show();
 ?>
 <script>
-function doPreview()
-{
+function doPreview(){
 	var ed = tinyMCE.get('campo_memo_2');
   	//ed.execCommand('mceToggleEditor',false,'content');
 }
 
-function fwSetHtmlEditorPreview(textAreaName)
-{
+function fwSetHtmlEditorPreview(textAreaName){
 	tinyMCE.init({
 		// General options
 		mode		: "exact",
