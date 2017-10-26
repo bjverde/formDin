6f7d8433e5d7b37d13b68aea0ba85a47d9de9929
Bjverde <bjverde@yahoo.com.br>
2017-10-26 00:38:52 -0200

    removendo arquivos de banco desnecessario

--- appexemplo_v2.5/banco/script_usuario_banco.sql
+++
@@ -1,3 +0,0 @@
-DROP USER IF EXISTS 'form_exemplo'@'localhost';
-CREATE USER 'form_exemplo'@'localhost' IDENTIFIED BY '123456';
-GRANT DELETE,EXECUTE,INSERT,SELECT,UPDATE ON form_exemplo.* TO 'form_exemplo'@'localhost';
