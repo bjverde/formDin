7f35a461754ca9e8e6a1efa51b65b60e0b2e7b8b
Bjverde <bjverde@yahoo.com.br>
2017-11-17 11:09:42 -0200

    MIGRATE_JQUERY Off

--- appexemplo_v1.0/index.php
+++ appexemplo_v1.0/index.php
@@ -38,7 +38,7 @@
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
-define('MIGRATE_JQUERY',TRUE);
+define('MIGRATE_JQUERY',FALSE);
 
 include('../base/classes/webform/TApplication.class.php');
 
