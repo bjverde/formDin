ff65cf20494190b5b1263a689f4bd70fc9039b24
Bjverde <bjverde@yahoo.com.br>
2017-11-22 23:05:04 -0200

    exemplo conexao

--- appexemplo_v1.0/includes/config_conexao.php
+++ appexemplo_v1.0/includes/config_conexao.php
@@ -39,10 +39,37 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
 
-define('BANCO','POSTGRES');
-define('HOST','localhost');
-define('PORT','5432');
-define('DATABASE','teste');
-define('USUARIO','postgres');
-define('SENHA','123456');
+/*
+ define('BANCO','FIREBIRD');
+ define('DATABASE','F://xampp//htdocs//www//base//exemplos//DBTESTE.GDB');
+ define('UTF8_DECODE',0);
+ define('USUARIO','SYSDBA');
+ define('SENHA','masterkey');
+ return;
+ */
+
+// sqlite
+define('BANCO','SQLITE');
+define('DATABASE','bdApoio.s3db');
+define('UTF8_DECODE',0);
+
+
+/*
+ define('BANCO','MYSQL');
+ define('HOST','127.0.0.1');
+ define('PORT','3306');
+ define('DATABASE','bdApoio');
+ define('USUARIO','root');
+ define('SENHA','');
+ */
+
+/*
+ // firebird
+ define('BANCO','FIREBIRD');
+ define('DATABASE','C://xampp//htdocs//formdin//base//exemplos//BDAPOIO.GDB');
+ define('UTF8_DECODE',0);
+ define('USUARIO','SYSDBA');
+ define('SENHA','masterkey');
+ */
+
 ?>
