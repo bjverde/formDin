023000407269c790d2e1b372a7f14c38683b7fc0
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2018-02-06 19:58:03 -0200

    Melhoria do exemplo

--- appexemplo_v1.0/view/form/exe_tform4_consulta_tree_p1.php
+++ appexemplo_v1.0/view/form/exe_tform4_consulta_tree_p1.php
@@ -1,9 +1,19 @@
 <?php
+function voIssetOrZero($attribute,$isTrue,$isFalse){
+    $retorno = $isFalse;
+    if(isset($attribute) && ($attribute<>'') ){
+        if( $attribute<>'0' ){
+            $retorno = $isTrue;
+        }
+    }
+    return $retorno;
+}
 
 $html1 = 'Esse form é um outra visão do form <i>"Mestre visão com Ajax"</i>.
           <br>
           <br>Este exemplo utiliza as tabelas tb_pedido e tb_pedido_item do banco de dados bdApoio.s3db ( sqlite )  ';
 
+$whereGrid = ' 1=1 ';
 $primaryKey = 'ID_PEDIDO';
 $frm = new TForm('Exemplo Form4 - Consulta Grid',600);
 $frm->setFlat(true);
@@ -18,6 +28,7 @@ $frm->addSelectField('forma_pagamento'	,'Forma Pagamento:',false,'1=Dinheiro,2=C
 $frm->addTextField('QTD', 'Quantidade de Itens',50,false);
 
 $frm->addButton('Ver Mestre visão com Ajax','redirectMestreAjax', null, null, null, true, false);
+$frm->addButton('Buscar', null, 'Buscar', null, null, false, false);
 $frm->addButton('Limpar', null, 'Limpar', null, null, false, false);
 
 $acao = isset($acao) ? $acao : null;
@@ -36,6 +47,14 @@ switch( $acao ) {
 		}
 	break;
 	//--------------------------------------------------------------------------------
+	case 'Buscar':
+	    $vo = new Vw_pedido_qtd_itensVO();
+	    $frm->setVo( $vo );
+	    $whereGrid = $whereGrid.( voIssetOrZero($vo->getNome_comprador(),' AND upper(NOME_COMPRADOR) like "%'.strtoupper($vo->getNome_comprador()).'%" ',null) );
+	    $whereGrid = $whereGrid.( voIssetOrZero($vo->getForma_pagamento(),' AND forma_pagamento='.$vo->getForma_pagamento().' ',null) );
+	    $whereGrid = $whereGrid.( voIssetOrZero($vo->getQtd(),' AND QTD='.$vo->getQtd().' ',null) );
+	    break;
+	//--------------------------------------------------------------------------------
 	case 'Limpar':
 		$frm->clearFields();
 	break;
@@ -49,7 +68,7 @@ switch( $acao ) {
 	break;
 }
 
-$dados = Vw_pedido_qtd_itensDAO::selectAll($primaryKey);
+$dados = Vw_pedido_qtd_itensDAO::selectAll($primaryKey,$whereGrid);
 $mixUpdateFields = $primaryKey.'|'.$primaryKey.',DATA_PEDIDO|DATA_PEDIDO,NOME_COMPRADOR|NOME_COMPRADOR,FORMA_PAGAMENTO|FORMA_PAGAMENTO,DES_FORMA_PAGAMENTO|DES_FORMA_PAGAMENTO,QTD|QTD';
 $gride = new TGrid( 'gd'        // id do gride
 				   ,'Gride'     // titulo do gride
