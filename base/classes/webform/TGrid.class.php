ac1a6369726a60ca585285926d0f5e7976cc3c78
Raphael Rodrigues Cavalcante <raphael.cavalcante@mpdft.mp.br>
2018-03-27 19:49:23 -0300

    :bug: Todos os campos apresentavam '...' independente do tamanho - corrigido

--- base/classes/webform/TGrid.class.php
+++ base/classes/webform/TGrid.class.php
@@ -571,8 +571,11 @@ class TGrid extends TTable
 							}
 							if ($objColumn->getColumnType() == 'columncompact') 
 							{
-							    $cell->setProperty('title', $tdValue); 
-							    $tdValue = substr($tdValue, 0, $objColumn->getMaxTextLength()).'...';
+							    if (strlen($tdValue)> $objColumn->getMaxTextLength())
+							    {
+							        $cell->setProperty('title', $tdValue); 
+    							    $tdValue = substr($tdValue, 0, $objColumn->getMaxTextLength()-3).' (...)';
+							    }
 							}
 							//$tdValue = isset($res[strtoupper($fieldName)][$k]) && $res[strtoupper($fieldName)][$k] ? $res[strtoupper($fieldName)][$k]: '<center>---</center>';
 							//$tdValue = ( $objColumn->getConvertHtml() ) ? htmlentities($tdValue): $tdValue;
