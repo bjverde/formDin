5c05a2a363d2c1e4e2b8ffbd6216659ee8ca2689
Bjverde <bjverde@yahoo.com.br>
2018-06-05 02:22:42 -0300

    BASE #54

--- base/classes/webform/TFone.class.php
+++ base/classes/webform/TFone.class.php
@@ -41,69 +41,62 @@
 
 class TFone extends TEdit
 {
-	/**
-	 * @param string $name
-	 * @param string $value
-	 * @param boolean $required
-	 */
-	public function __construct($strName,$strValue=null,$boolRequired=null)
-	{
-		parent::__construct($strName,$strValue,18,$boolRequired);
-		$this->setFieldType('fone');
-		$this->addEvent('onkeyup','fwFormatarTelefone(this)');
-		// aplicar formatação ao exibir.
-		$js = new TElement('script');
-		$js->setProperty('type',"text/javascript");
-		$js->add('fwFormatarTelefone(fwGetObj("'.$this->getId().'"));');
-		$this->add($js);
-	}
-	public function getFormated()
-	{
+    /**
+     * @param string  $name
+     * @param string  $value
+     * @param boolean $required
+     */
+    public function __construct($strName,$strValue=null,$boolRequired=null)
+    {
+        parent::__construct($strName, $strValue, 18, $boolRequired);
+        $this->setFieldType('fone');
+        $this->addEvent('onkeyup', 'fwFormatarTelefone(this)');
+        // aplicar formatação ao exibir.
+        $js = new TElement('script');
+        $js->setProperty('type', "text/javascript");
+        $js->add('fwFormatarTelefone(fwGetObj("'.$this->getId().'"));');
+        $this->add($js);
+    }
+    public function getFormated()
+    {
 
-		if( $this->getValue() )
-		{
-		    $value = preg_replace("/[^0-9]/","",$this->getValue());
-			// nao pode comecar com zero
-			while ( substr($value,0,1)==="0" )
-			{
-				$value=substr($value,1);
-			}
-			if ( ! $value) // nenhum valor informado
-			{
-				return null;
-			}
-			if ( strlen($value) < 5 ) // não precisa formatar
-			{
-				return $value;
-			}
-			if ( substr($value,0,4) == '0800' )
-			{
-				print 'luis';
-				$value = substr($value,0,4)." " . substr($value,4,3) . " " . substr($value,7);
-			}
-			else if (strlen($value) < 8 )
-			{
-				$value = substr($value,0,3).'-'.substr($value,3);
-			}
-			else if (strlen($value) == 8 )
-			{
-				$value=substr($value,0,4).'-'.substr($value,4);
-			}
-			elseif (strlen($value)==9)
-			{
-				$value='(0xx'.substr($value,0,2).') '.substr($value,2,3).'-'.substr($value,5);
-			}
-			elseif (strlen($value)==10)
-			{
-				$value='(0xx'.substr($value,1,2).') '.substr($value,3,3).'-'.substr($value,6);
-			}
-			elseif (strlen($value) > 10 )
-			{
-				$value='(0xx'.substr($value,1,2).') '.substr($value,3,4).'-'.substr($value,7);
-			}
-			return $value;
-		}
-	}
+        if($this->getValue() ) {
+            $value = preg_replace("/[^0-9]/", "", $this->getValue());
+            // nao pode comecar com zero
+            while ( substr($value, 0, 1)==="0" )
+            {
+                $value=substr($value, 1);
+            }
+            if (! $value) // nenhum valor informado
+            {
+                return null;
+            }
+            if (strlen($value) < 5 ) // não precisa formatar
+            {
+                return $value;
+            }
+            if (substr($value, 0, 4) == '0800' ) {
+                print 'luis';
+                $value = substr($value, 0, 4)." " . substr($value, 4, 3) . " " . substr($value, 7);
+            }
+            else if (strlen($value) < 8 ) {
+                $value = substr($value, 0, 3).'-'.substr($value, 3);
+            }
+            else if (strlen($value) == 8 ) {
+                $value=substr($value, 0, 4).'-'.substr($value, 4);
+            }
+            elseif (strlen($value)==9) {
+                $value='(0xx'.substr($value, 0, 2).') '.substr($value, 2, 3).'-'.substr($value, 5);
+            }
+            elseif (strlen($value)==10) {
+                $value='(0xx'.substr($value, 1, 2).') '.substr($value, 3, 3).'-'.substr($value, 6);
+            }
+            elseif (strlen($value) > 10 ) {
+                $value='(0xx'.substr($value, 1, 2).') '.substr($value, 3, 4).'-'.substr($value, 7);
+            }
+            return $value;
+        }
+    }
 }
 //$val = new TFone('des_telefone','6234683581',50);
 //$val->show();
