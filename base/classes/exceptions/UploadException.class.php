da096e0e8f70ae70fcdc5f2e356fc3158b3776e3
Bjverde <bjverde@yahoo.com.br>
2018-06-06 00:39:26 -0300

    :abc: BASE #54 phpcs, phpcbf and php-cs-fixer-v2

--- base/classes/exceptions/UploadException.class.php
+++ base/classes/exceptions/UploadException.class.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * FormDin or Dynamic Form is a php Framework for creating web system quickly and easily.
+ *
+ * @author  Bjverde <bjverde@yahoo.com.br>
+ * @license https://github.com/bjverde/formDin/blob/master/LICENSE GPL-3.0
+ * @link    https://github.com/bjverde/formDin
+ *
+ * PHP Version 5.6
+ */
 class UploadException extends Exception {
     public function __construct($code) {
         $message = $this->codeToMessage($code);
