773f3049133a181481870a3d9d358d658dcb9ae4
Bjverde <bjverde@yahoo.com.br>
2018-06-17 11:37:07 -0300

    :abc: APP #54 phpcs, phpcbf PSR1, PSR2

--- base/includes/acoes_ajax.php
+++ base/includes/acoes_ajax.php
@@ -5,32 +5,32 @@
  * Copyright (C) 2012 Ministério do Planejamento
  * ----------------------------------------------------------------------------
  * This file is part of Formdin Framework.
- * 
+ *
  * Formdin Framework is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License version 3
  * along with this program; if not,  see <http://www.gnu.org/licenses/>
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
  * Este arquivo é parte do Framework Formdin.
- * 
+ *
  * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
  * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
  * do Software Livre (FSF).
- * 
+ *
  * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
  * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
  * para maiores detalhes.
- * 
+ *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
  * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
@@ -42,96 +42,82 @@ session_start();
 /*
 if ( isset($_SESSION['ACESSO_PUBLICO']) && $_SESSION['ACESSO_PUBLICO'] == true )
 {
-	include("../../includes/config_sem_autenticar.inc");
+    include("../../includes/config_sem_autenticar.inc");
 }
 else
 {
-	include("../../includes/config.inc");
+    include("../../includes/config.inc");
 }
 */
 include("conexao.inc");
 
 $acao = $_REQUEST['acao'];
-switch( strtoupper( $acao ) )
-{
-	case 'UF_MUNICIPIO':
-		$_POST['colunaFiltro']			= 'COD_UF';
-		$_POST['pacoteOracle']			= 'SIGER.PKG_GERAL.SEL_MUNICIPIO';
-		$_POST['colunaCodigo']			= 'COD_MUNICIPIO';
-		$_POST['colunaDescricao']		= 'NOM_MUNICIPIO';
-		$_POST['descPrimeiraOpcao']	= '-- selecione --';
-		$_POST['valorPrimeiraOpcao']	= '';
-		$_POST['descNenhumaOpcao']		= '-- vazio --';
-	break;
-	case 'MUNICIPIO_BIOMA':
-		$_POST['colunaFiltro']			= 'COD_MUNICIPIO';
-		$_POST['pacoteOracle']			= 'SIGER.PKG_TIPO_BIOMA.SEL_BIOMA_UF';
-		$_POST['colunaCodigo']			= 'SEQ_TIPO_BIOMA_UF';
-		$_POST['colunaDescricao']		= 'DES_TIPO_BIOMA';
-		$_POST['descPrimeiraOpcao']	= '-- selecione --';
-		$_POST['valorPrimeiraOpcao']	= '';
-		$_POST['descNenhumaOpcao']		= '-- vazio --';
-	break;
+switch (strtoupper($acao)) {
+    case 'UF_MUNICIPIO':
+        $_POST['colunaFiltro']          = 'COD_UF';
+        $_POST['pacoteOracle']          = 'SIGER.PKG_GERAL.SEL_MUNICIPIO';
+        $_POST['colunaCodigo']          = 'COD_MUNICIPIO';
+        $_POST['colunaDescricao']       = 'NOM_MUNICIPIO';
+        $_POST['descPrimeiraOpcao'] = '-- selecione --';
+        $_POST['valorPrimeiraOpcao']    = '';
+        $_POST['descNenhumaOpcao']      = '-- vazio --';
+        break;
+    case 'MUNICIPIO_BIOMA':
+        $_POST['colunaFiltro']          = 'COD_MUNICIPIO';
+        $_POST['pacoteOracle']          = 'SIGER.PKG_TIPO_BIOMA.SEL_BIOMA_UF';
+        $_POST['colunaCodigo']          = 'SEQ_TIPO_BIOMA_UF';
+        $_POST['colunaDescricao']       = 'DES_TIPO_BIOMA';
+        $_POST['descPrimeiraOpcao'] = '-- selecione --';
+        $_POST['valorPrimeiraOpcao']    = '';
+        $_POST['descNenhumaOpcao']      = '-- vazio --';
+        break;
 }
 
 // executar pacote
 $bvars=array(strtoupper($_POST['colunaFiltro'])=>$_POST['valorFiltro']);
-if((string)$_POST['campoFormFiltro']<>'')
-{
-	$aCampos 	= explode('|',$_POST['campoFormFiltro']);
-	$aValores 	= explode('|',$_POST['campoFormFiltroValor']);
-	foreach($aCampos as $k=>$v)
-	{
-		if((string)$aValores[$k]<>'')
-		{
-			$bvars[$v]=$aValores[$k];
-		}
-	}
+if ((string)$_POST['campoFormFiltro']<>'') {
+    $aCampos    = explode('|', $_POST['campoFormFiltro']);
+    $aValores   = explode('|', $_POST['campoFormFiltroValor']);
+    foreach ($aCampos as $k => $v) {
+        if ((string)$aValores[$k]<>'') {
+            $bvars[$v]=$aValores[$k];
+        }
+    }
 }
 // executar pacote
-$pacoteCache = explode('|',$_POST['pacoteOracle']);
-if (!isset($pacoteCache[1]))
-{
-	$pacoteCache[1] = NULL;
+$pacoteCache = explode('|', $_POST['pacoteOracle']);
+if (!isset($pacoteCache[1])) {
+    $pacoteCache[1] = null;
 }
-if( $erro = recuperarPacote($pacoteCache[0],$bvars,$res,$pacoteCache[1]))
-{
-	print "alert('Erro na função combinarSelect().\\n".$erro[0]."')";
-	return;
+if ($erro = recuperarPacote($pacoteCache[0], $bvars, $res, $pacoteCache[1])) {
+    print "alert('Erro na função combinarSelect().\\n".$erro[0]."')";
+    return;
 }
-$campoCodigo 	 = strtoupper($_POST['colunaCodigo']);
+$campoCodigo     = strtoupper($_POST['colunaCodigo']);
 $campoDescricao = strtoupper($_POST['colunaDescricao']);
 $retorno='{"campo":"'.$_POST['campoSelect'].
-			'","valorInicial":"'.$_POST['valorInicial'].
-			'","selectFilhoStatus":"'.$_POST['selectFilhoStatus'].
-			'","descPrimeiraOpcao":"'.$_POST['descPrimeiraOpcao'].
-			'","valorPrimeiraOpcao":"'.$_POST['valorPrimeiraOpcao'].
-			'","descNenhumaOpcao":"'.$_POST['descNenhumaOpcao'].'"';
+            '","valorInicial":"'.$_POST['valorInicial'].
+            '","selectFilhoStatus":"'.$_POST['selectFilhoStatus'].
+            '","descPrimeiraOpcao":"'.$_POST['descPrimeiraOpcao'].
+            '","valorPrimeiraOpcao":"'.$_POST['valorPrimeiraOpcao'].
+            '","descNenhumaOpcao":"'.$_POST['descNenhumaOpcao'].'"';
 
-if($res)
-{
-	if( !array_key_exists($campoCodigo,$res))
-	{
-		print "alert('Erro nos parametros da função combinarSelect().\\nA coluna ".$campoCodigo." não existe no retorno da função \\n".$_POST['pacoteOracle']."')";
-		return;
-	}
-	else if( !array_key_exists($campoDescricao,$res))
-	{
-		print "alert('Erro nos parametros da função combinarSelect().\\nA coluna ".$campoDescricao." não existe no retorno da função \\n".$_POST['pacoteOracle']."')";
-		return;
-	}
-	if($res){
-		$retorno.=',"dados":{';
-		foreach($res[$campoCodigo] as $k=>$v)
-		{
-			$retorno .= ($k>0) ? ',' : '';
-			$retorno.='"'.$v.'":"'.str_replace('"','“',$res[$campoDescricao][$k]).'"';
-		}
-		$retorno.="}";
-	}
+if ($res) {
+    if (!array_key_exists($campoCodigo, $res)) {
+        print "alert('Erro nos parametros da função combinarSelect().\\nA coluna ".$campoCodigo." não existe no retorno da função \\n".$_POST['pacoteOracle']."')";
+        return;
+    } elseif (!array_key_exists($campoDescricao, $res)) {
+        print "alert('Erro nos parametros da função combinarSelect().\\nA coluna ".$campoDescricao." não existe no retorno da função \\n".$_POST['pacoteOracle']."')";
+        return;
+    }
+    if ($res) {
+        $retorno.=',"dados":{';
+        foreach ($res[$campoCodigo] as $k => $v) {
+            $retorno .= ($k>0) ? ',' : '';
+            $retorno.='"'.$v.'":"'.str_replace('"', '“', $res[$campoDescricao][$k]).'"';
+        }
+        $retorno.="}";
+    }
 }
 $retorno .= ($retorno == '') ? '' : '}';
 print $retorno;
-?>
-
-
--- base/includes/dbm_ler_documentacao.php
+++ base/includes/dbm_ler_documentacao.php
@@ -5,32 +5,32 @@
  * Copyright (C) 2012 Ministério do Planejamento
  * ----------------------------------------------------------------------------
  * This file is part of Formdin Framework.
- * 
+ *
  * Formdin Framework is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License version 3
  * along with this program; if not,  see <http://www.gnu.org/licenses/>
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
  * Este arquivo é parte do Framework Formdin.
- * 
+ *
  * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
  * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
  * do Software Livre (FSF).
- * 
+ *
  * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
  * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
  * para maiores detalhes.
- * 
+ *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
  * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
@@ -38,34 +38,28 @@
  */
 
 /*
-	Recuperação da documentação via ajax
+    Recuperação da documentação via ajax
 */
 sleep(1);
-$dir  	= 'doc/';
-if( !file_exists($dir))
-{
-	for($i=1;$i<11;$i++)
-	{
-		$dir = '../'.$dir;
-		if( file_exists($dir))
-		{
-			break;
-		}
-	}
-	
-	if( !file_exists($dir))
-	{
-		print 'Diretório para gravação dos dados ['.$dir.'] não encontrado!';
-		return;
-	}
+$dir    = 'doc/';
+if (!file_exists($dir)) {
+    for ($i=1; $i<11; $i++) {
+        $dir = '../'.$dir;
+        if (file_exists($dir)) {
+            break;
+        }
+    }
+    
+    if (!file_exists($dir)) {
+        print 'Diretório para gravação dos dados ['.$dir.'] não encontrado!';
+        return;
+    }
 }
-$type	= 'db4';
-$file 	= 'documentacao.'.$type;
-$key  	= md5( strtoupper($_REQUEST['modulo'].'_'.$_REQUEST['campo']));
-$dbh = dba_open($dir.$file,'c',$type) or die('Erro');
-if (dba_exists($key,$dbh)) 
-{
-	print dba_fetch($key,$dbh);
+$type   = 'db4';
+$file   = 'documentacao.'.$type;
+$key    = md5(strtoupper($_REQUEST['modulo'].'_'.$_REQUEST['campo']));
+$dbh = dba_open($dir.$file, 'c', $type) or die('Erro');
+if (dba_exists($key, $dbh)) {
+    print dba_fetch($key, $dbh);
 }
 dba_close($dbh);
-?>
