996d452a8e18f60233f606936a0ab187119faee1
Bjverde <bjverde@yahoo.com.br>
2018-06-23 18:24:23 -0300

    :memo: APP #62 melhorando documentação

--- appexemplo_v1.0/view/form/exe_TForm5.php
+++ appexemplo_v1.0/view/form/exe_TForm5.php
@@ -14,6 +14,8 @@ $frm->addHiddenField( $primaryKey );   // coluna chave da tabela
 $frm->addTextField('TXNOME', 'Nome do Texto',50,true);
 $frm->addDateField('TXDATA', 'Data',50,true);
 $frm->addRadioField('STATIVO' , 'Ativo:',true,'S=SIM,N=Não',null,false,null,2,null,null,null,false);//->addEvent('onDblclick','dblClick(this)');
+$html = 'Instruções. O campo Memo com TinyMCE só funciona com a função de BakcEditor habilita. O campo deve ser obrigatorio para envitar possiveis problemas.';
+$frm->addHtmlField('html1',$html,null,'Dica:',null,400)->setCss('border','1px dashed blue');
 
 $frm->addMemoField('TEXTO' ,'Texto',10000,false,100,15,true,true,false);
 $frm->setRichEdit(true);
