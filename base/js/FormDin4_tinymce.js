25c776bc445a4a1de3445b20bde00ffe0c873ed0
Bjverde <bjverde@yahoo.com.br>
2018-06-24 03:05:49 -0300

    :memo: BASE #62 pequena melhorias retirando pluging falhos

--- base/js/FormDin4_tinymce.js
+++ base/js/FormDin4_tinymce.js
@@ -65,28 +65,30 @@ function fwSetHtmlEditor(textAreaName,saveHandler,readonly,height,width)
 			mode		: "exact",
 			elements 	: textAreaName,
 			theme 		: "advanced",
+			language	: "pt",
 			readonly 	: readonly,
-			//height		: height,
+			//height    : height,
 			//width		: width,
-			auto_resize	: true,
-			mce_windowresize:true,
+			//auto_resize	: true,
+			//mce_windowresize:true,
 			//editor_selector : "mceSimple"// aplicar utilizando a propriedade class do elemento
 			//theme_advanced_resize_horizontal : false,
 			//theme_advanced_resize_vertical : false,
-			theme_advanced_resizing : false,
+			theme_advanced_resizing : true,
 			//plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager,tabfocus",
 			plugins : "safari,spellchecker,pagebreak,style,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,tabfocus",
 			tab_focus : ':prev,:next',
 			// Theme options
+			// removido image media fullscreen
 			theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,advhr,|,print,|,ltr,rtl",
 			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
 			theme_advanced_toolbar_location : "top", // external
 			theme_advanced_toolbar_align : "left",
 			theme_advanced_statusbar_location : "bottom",
-			plugin_preview_width : width,
-			plugin_preview_height : height,
+			//plugin_preview_width : width,
+			//plugin_preview_height : height,
 
 
 			// Style formats
