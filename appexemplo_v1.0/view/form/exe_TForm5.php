0b18b65372f750b8c9246e1a301635e9959d4987
Bjverde <bjverde@yahoo.com.br>
2018-06-24 00:49:41 -0300

    :memo: APP #62 melhorando documentação

--- appexemplo_v1.0/view/form/exe_TForm5.php
+++ appexemplo_v1.0/view/form/exe_TForm5.php
@@ -19,7 +19,11 @@ $frm->addHtmlField('html1',$html,null,'Dica:',null,400)->setCss('border','1px da
 
 $frm->addMemoField('TEXTO' ,'Texto',10000,false,100,15,true,true,false);
 $frm->setRichEdit(true);
-$frm->addJavascript('fwSetHtmlEditor("TEXTO","callBackEditor",false)');
+//Metodo customizado
+$frm->addJavascript('fwSetHtmlEditorPreview("TEXTO")');
+
+//$frm->addJavascript('fwSetHtmlEditor("TEXTO","callBackEditor",false)');
+
 
 echo 'Valor do Campo: TEXTO =<br>';
 echo htmlspecialchars($frm->get('TEXTO'));
@@ -121,4 +125,41 @@ function callBackEditor(ed) {
           error: function( res ){ alert('Erro!\n\n'+res );fwUnBlockScreen(); }
     });
 }
+//-----------------------------------------------------------------------------------------------
+function fwSetHtmlEditorPreview(textAreaName){
+	tinyMCE.init({
+		// General options
+		mode		: "exact",
+		elements 	: textAreaName,
+		language	:'pt',   		
+		theme 		: "advanced",
+		//skin 		: "o2k7",
+        //skin_variant : "silver",
+		
+		plugins 	: "safari,spellchecker,pagebreak,style,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,tabfocus",
+		
+		// Theme options
+		/*theme_advanced_buttons1 : "preview",
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+       	theme_advanced_buttons2 : "",
+		theme_advanced_buttons3 : "",
+		theme_advanced_buttons4 : "",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom"
+		*/
+	 	content_css : pastaBase+"css/tinyMCE.css",
+
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+
+	})
+}
+//-----------------------------------------------------------------------------------------------
 </script>
