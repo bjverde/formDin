9af1e312c23fab47609f951dde0a2c28509eaa80
Bjverde <bjverde@yahoo.com.br>
2018-11-21 00:45:29 -0200

    :memo: APP #87

--- appexemplo_v2.5/modulos/acesso_perfil.php
+++ appexemplo_v2.5/modulos/acesso_perfil.php
@@ -1,66 +1,142 @@
 <?php
+defined('APLICATIVO') or die();
+
 $primaryKey = 'IDPERFIL';
-$frm = new TForm('Cadastro de Perfils', 600);
+$frm = new TForm('Cadastro de Perfils', 400);
 $frm->setFlat(true);
 $frm->setMaximize(true);
 
 
-$frm->addHiddenField($primaryKey); // coluna chave da tabela
+$frm->addHiddenField( 'BUSCAR' ); //Campo oculto para buscas
+$frm->addHiddenField( $primaryKey );   // coluna chave da tabela
 $frm->addTextField('NOM_PERFIL', 'Nome Perfil', 50, true);
 $frm->addSelectField('SIT_ATIVO', 'Ativo:', true, 'S=Sim,N=Não', true);
-$frm->addTextField('DAT_INCLUSAO', 'Data Inclusão:', null, false, null, null, true)->setReadOnly(true);
-$frm->addTextField('DAT_UPDATE', 'Data Update: ', null, false, null, null, false)->setReadOnly(true);
+$dti = $frm->addDateField('DAT_INCLUSAO', 'Data Inclusão:');
+$dtu = $frm->addDateField('DAT_UPDATE', 'Data Update:');
+$dti->setToolTip('Não será usado cadastro');
+$dtu->setToolTip('Não será usado cadastro');
+
+$frm->addButton('Buscar', null, 'btnBuscar', 'buscar()', null, true, false);
+$frm->addButton('Salvar', null, 'Salvar', null, null, false, false);
+$frm->addButton('Limpar', null, 'Limpar', null, null, false, false);
+
 
 $acao = isset($acao) ? $acao : null;
 switch( $acao ) {
-case 'Salvar':
-    if ($frm->validate() ) {
-        $vo = new Acesso_perfilVO();
-        $frm->setVo($vo);
-        $resultado = Acesso_perfilDAO::insert($vo);
-        if($resultado==1) {
-            $frm->setMessage('Registro gravado com sucesso!!!');
-            $frm->clearFields();
-        }else{
-            $frm->setMessage($resultado);
-        }
-    }
-    break;
-    //--------------------------------------------------------------------------------
-case 'Limpar':
-    $frm->clearFields();
-    break;
-    //--------------------------------------------------------------------------------
-case 'gd_excluir':
-    $id = $frm->get($primaryKey);
-    $resultado = Acesso_perfilDAO::delete($id);;
-    if($resultado==1) {
-        $frm->setMessage('Registro excluido com sucesso!!!');
-        $frm->clearFields();
-    }else{
-        $frm->clearFields();
-        $frm->setMessage($resultado);
-    }
-    break;
+	case 'Salvar':
+		try{
+			if ( $frm->validate() ) {
+				$vo = new Acesso_perfilVO();
+				$frm->setVo( $vo );
+				$resultado = Acesso_perfil::save( $vo );
+				if($resultado==1) {
+					$frm->setMessage('Registro gravado com sucesso!!!');
+					$frm->clearFields();
+				}else{
+					$frm->setMessage($resultado);
+				}
+			}
+		}
+		catch (DomainException $e) {
+			$frm->setMessage( $e->getMessage() );
+		}
+		catch (Exception $e) {
+			MessageHelper::logRecord($e);
+			$frm->setMessage( $e->getMessage() );
+		}
+	break;
+	//--------------------------------------------------------------------------------
+	case 'Limpar':
+		$frm->clearFields();
+	break;
+	//--------------------------------------------------------------------------------
+	case 'gd_excluir':
+		try{
+			$id = $frm->get( $primaryKey ) ;
+			$resultado = Acesso_perfil::delete( $id );;
+			if($resultado==1) {
+				$frm->setMessage('Registro excluido com sucesso!!!');
+				$frm->clearFields();
+			}else{
+				$frm->clearFields();
+				$frm->setMessage($resultado);
+			}
+		}
+		catch (DomainException $e) {
+			$frm->setMessage( $e->getMessage() );
+		}
+		catch (Exception $e) {
+			MessageHelper::logRecord($e);
+			$frm->setMessage( $e->getMessage() );
+		}
+	break;
 }
 
-$dados = Acesso_perfilDAO::selectAll($primaryKey);
-$mixUpdateFields = $primaryKey.'|'.$primaryKey.',NOM_PERFIL|NOM_PERFIL,SIT_ATIVO|SIT_ATIVO,DAT_INCLUSAO|DAT_INCLUSAO,DAT_UPDATE|DAT_UPDATE';
-$gride = new TGrid(
-    'gd'        // id do gride
-, 'Gride'     // titulo do gride
-, $dados           // array de dados
-, null          // altura do gride
-, null          // largura do gride
-, $primaryKey   // chave primaria
-, $mixUpdateFields
-);
-$gride->addColumn($primaryKey, 'id', 50, 'center');
-$gride->addColumn('NOM_PERFIL', 'Nome Perfil', 50, 'center');
-$gride->addColumn('SIT_ATIVO', 'Ativo', 30, 'center');
-$gride->addColumn('DAT_INCLUSAO', 'Data Inclusão', 100, 'center');
-$gride->addColumn('DAT_UPDATE', 'Data Update', 100, 'center');
-$frm->addHtmlField('gride', $gride);
-$frm->setAction('Salvar,Limpar');
+
+function getWhereGridParameters(&$frm){
+	$retorno = null;
+	if($frm->get('BUSCAR') == 1 ){
+		$retorno = array(
+				'IDPERFIL'=>$frm->get('IDPERFIL')
+				,'NOM_PERFIL'=>$frm->get('NOM_PERFIL')
+				,'SIT_ATIVO'=>$frm->get('SIT_ATIVO')
+				,'DAT_INCLUSAO'=>$frm->get('DAT_INCLUSAO')
+				,'DAT_UPDATE'=>$frm->get('DAT_UPDATE')
+		);
+	}
+	return $retorno;
+}
+
+if( isset( $_REQUEST['ajax'] )  && $_REQUEST['ajax'] ) {
+	$maxRows = ROWS_PER_PAGE;
+	$whereGrid = getWhereGridParameters($frm);
+	$page = PostHelper::get('page');
+	$dados = Acesso_perfil::selectAllPagination( $primaryKey, $whereGrid, $page,  $maxRows);
+	$realTotalRowsSqlPaginator = Acesso_perfil::selectCount( $whereGrid );
+	$mixUpdateFields = $primaryKey.'|'.$primaryKey
+					.',NOM_PERFIL|NOM_PERFIL'
+					.',SIT_ATIVO|SIT_ATIVO'
+					.',DAT_INCLUSAO|DAT_INCLUSAO'
+					.',DAT_UPDATE|DAT_UPDATE'
+					;
+	$gride = new TGrid( 'gd'                        // id do gride
+					   ,'Gride with SQL Pagination' // titulo do gride
+					   );
+	$gride->addKeyField( $primaryKey ); // chave primaria
+	$gride->setData( $dados ); // array de dados
+	$gride->setRealTotalRowsSqlPaginator( $realTotalRowsSqlPaginator );
+	$gride->setMaxRows( $maxRows );
+	$gride->setUpdateFields($mixUpdateFields);
+	$gride->setUrl( 'acesso_perfil.php' );
+
+	$gride->addColumn($primaryKey,'id');
+	$gride->addColumn('NOM_PERFIL','Nome Perfil');
+	$gride->addColumn('SIT_ATIVO', 'Ativo', 30, 'center');
+	$gride->addColumn('DAT_INCLUSAO', 'Data Inclusão', 100, 'center');
+	$gride->addColumn('DAT_UPDATE', 'Data Update', 100, 'center');
+
+	$gride->show();
+	die();
+}
+
+$frm->addHtmlField('gride');
+$frm->addJavascript('init()');
 $frm->show();
+
 ?>
+<script>
+function init() {
+	var Parameters = {"BUSCAR":""
+					,"IDPERFIL":""
+					,"NOM_PERFIL":""
+					,"SIT_ATIVO":""
+					,"DAT_INCLUSAO":""
+					,"DAT_UPDATE":""
+					};
+	fwGetGrid('acesso_perfil.php','gride',Parameters,true);
+}
+function buscar() {
+	jQuery("#BUSCAR").val(1);
+	init();
+}
+</script>
