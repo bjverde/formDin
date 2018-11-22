93f2632bd4a21d650f1b5c935da58fc1b07accc8
Bjverde <bjverde@yahoo.com.br>
2018-11-22 00:26:49 -0200

    :memo: APP #87 update DAO

--- appexemplo_v2.0/dao/AutoridadeDAO.class.php
+++ appexemplo_v2.0/dao/AutoridadeDAO.class.php
@@ -1,82 +1,106 @@
 <?php
-class AutoridadeDAO extends TPDOConnection
-{
+class AutoridadeDAO extends TPDOConnection {
 
-    public static function insert( AutoridadeVO $objVo )
-    {
-        if($objVo->getIdautoridade() ) {
-            return self::update($objVo);
-        }
-        $values = array(  $objVo->getDat_evento() 
-         , $objVo->getOrdem() 
-         , $objVo->getCargo() 
-         , $objVo->getNome_pessoa() 
-         );
-        return self::executeSql(
-            'insert into autoridade(
-								 dat_evento
-								,ordem
-								,cargo
-								,nome_pessoa
-								) values (?,?,?,?)', $values 
-        );
-    }
-    //--------------------------------------------------------------------------------
-    public static function delete( $id )
-    {
-        $values = array($id);
-        return self::executeSql('delete from autoridade where idautoridade = ?', $values);
-    }
-    //--------------------------------------------------------------------------------
-    public static function select( $id )
-    {
-        $values = array($id);
-        return self::executeSql(
-            'select
-								 idautoridade
-								,dat_inclusao
+	private static $sqlBasicSelect = 'select
+									  idautoridade
+									 ,dat_inclusao
+									 ,dat_evento
+									 ,ordem
+									 ,cargo
+									 ,nome_pessoa
+									 from form_exemplo.autoridade ';
+
+	private static function processWhereGridParameters( $whereGrid ) {
+		$result = $whereGrid;
+		if ( is_array($whereGrid) ){
+			$where = ' 1=1 ';
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'IDAUTORIDADE', SqlHelper::SQL_TYPE_NUMERIC);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'DAT_INCLUSAO', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'DAT_EVENTO', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'ORDEM', SqlHelper::SQL_TYPE_NUMERIC);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'CARGO', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'NOME_PESSOA', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$result = $where;
+		}
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectById( $id ) {
+		if( empty($id) || !is_numeric($id) ){
+			throw new InvalidArgumentException();
+		}
+		$values = array($id);
+		$sql = self::$sqlBasicSelect.' where idautoridade = ?';
+		$result = self::executeSql($sql, $values );
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectCount( $where=null ){
+		$where = self::processWhereGridParameters($where);
+		$sql = 'select count(idautoridade) as qtd from form_exemplo.autoridade';
+		$sql = $sql.( ($where)? ' where '.$where:'');
+		$result = self::executeSql($sql);
+		return $result['QTD'][0];
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectAllPagination( $orderBy=null, $where=null, $page=null,  $rowsPerPage= null ) {
+		$rowStart = PaginationSQLHelper::getRowStart($page,$rowsPerPage);
+		$where = self::processWhereGridParameters($where);
+
+		$sql = self::$sqlBasicSelect
+		.( ($where)? ' where '.$where:'')
+		.( ($orderBy) ? ' order by '.$orderBy:'')
+		.( ' LIMIT '.$rowStart.','.$rowsPerPage);
+
+		$result = self::executeSql($sql);
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectAll( $orderBy=null, $where=null ) {
+		$where = self::processWhereGridParameters($where);
+		$sql = self::$sqlBasicSelect
+		.( ($where)? ' where '.$where:'')
+		.( ($orderBy) ? ' order by '.$orderBy:'');
+
+		$result = self::executeSql($sql);
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function insert( AutoridadeVO $objVo ) {
+		$values = array(  $objVo->getDat_inclusao() 
+						, $objVo->getDat_evento() 
+						, $objVo->getOrdem() 
+						, $objVo->getCargo() 
+						, $objVo->getNome_pessoa() 
+						);
+		return self::executeSql('insert into form_exemplo.autoridade(
+								 dat_inclusao
 								,dat_evento
 								,ordem
 								,cargo
 								,nome_pessoa
-								from autoridade where idautoridade = ?', $values 
-        );
-    }
-    //--------------------------------------------------------------------------------
-    public static function selectAll( $orderBy=null, $where=null )
-    {
-        return self::executeSql(
-            'select
-								 idautoridade
-								,dat_inclusao
-								,dat_evento
-								,ordem
-								,cargo
-								,nome_pessoa
-								from autoridade'.
-            ( ($where)? ' where '.$where:'').
-            ( ($orderBy) ? ' order by '.$orderBy:'')
-        );
-    }
-    //--------------------------------------------------------------------------------
-    public static function update( AutoridadeVO $objVo )
-    {
-        $values = array( $objVo->getDat_inclusao()
-         ,$objVo->getDat_evento()
-         ,$objVo->getOrdem()
-         ,$objVo->getCargo()
-         ,$objVo->getNome_pessoa()
-         ,$objVo->getIdautoridade() );
-        return self::executeSql(
-            'update autoridade set 
+								) values (?,?,?,?,?)', $values );
+	}
+	//--------------------------------------------------------------------------------
+	public static function update ( AutoridadeVO $objVo ) {
+		$values = array( $objVo->getDat_inclusao()
+						,$objVo->getDat_evento()
+						,$objVo->getOrdem()
+						,$objVo->getCargo()
+						,$objVo->getNome_pessoa()
+						,$objVo->getIdautoridade() );
+		return self::executeSql('update form_exemplo.autoridade set 
 								 dat_inclusao = ?
 								,dat_evento = ?
 								,ordem = ?
 								,cargo = ?
 								,nome_pessoa = ?
-								where idautoridade = ?', $values
-        );
-    }
-    //--------------------------------------------------------------------------------
+								where idautoridade = ?',$values);
+	}
+	//--------------------------------------------------------------------------------
+	public static function delete( $id ){
+		$values = array($id);
+		return self::executeSql('delete from form_exemplo.autoridade where idautoridade = ?',$values);
+	}
 }
 ?>
--- appexemplo_v2.0/dao/UfDAO.class.php
+++ appexemplo_v2.0/dao/UfDAO.class.php
@@ -1,88 +1,94 @@
 <?php
-class UfDAO extends TPDOConnection
-{
+class UfDAO extends TPDOConnection {
 
-    private static $sqlBasicSelect = 'select
+	private static $sqlBasicSelect = 'select
 									  cod_uf
 									 ,nom_uf
 									 ,sig_uf
 									 ,cod_regiao
-									 from uf ';
+									 from form_exemplo.uf ';
 
-    public static function selectCount()
-    {
-        $sql = 'select count(cod_uf) as qtd from uf';
-        $result = self::executeSql($sql);
-        return $result['QTD'][0];
-    }
-    //--------------------------------------------------------------------------------
-    public static function select( $id ) 
-    {
-        $values = array($id);
-        $sql = self::$sqlBasicSelect.' where cod_uf = ?';
-        $result = self::executeSql($sql, $values);
-        return $result;
-    }
-    //--------------------------------------------------------------------------------
-    public static function selectAllPagination( $orderBy=null, $where=null, $page=null,  $rowsPerPage= null) 
-    {
-        $rowStart = PaginationSQLHelper::getRowStart($page, $rowsPerPage);
-        
-        $sql = self::$sqlBasicSelect
-        .( ($where)? ' where '.$where:'')
-        .( ($orderBy) ? ' order by '.$orderBy:'')
-        .( ' LIMIT '.$rowStart.','.$rowsPerPage);
-        
-        $result = self::executeSql($sql);
-        return $result;
-    }
-    //--------------------------------------------------------------------------------
-    public static function selectAll( $orderBy=null, $where=null ) 
-    {
-        $sql = self::$sqlBasicSelect
-        .( ($where)? ' where '.$where:'')
-        .( ($orderBy) ? ' order by '.$orderBy:'');
-        $result = self::executeSql($sql);
-        return $result;
-    }
-    //--------------------------------------------------------------------------------
-    public static function insert( UfVO $objVo ) 
-    {
-        if($objVo->getCod_uf() ) {
-            return self::update($objVo);
-        }
-        $values = array(  $objVo->getNom_uf() 
-         , $objVo->getSig_uf() 
-         , $objVo->getCod_regiao() 
-         );
-        return self::executeSql(
-            'insert into uf(
+	private static function processWhereGridParameters( $whereGrid ) {
+		$result = $whereGrid;
+		if ( is_array($whereGrid) ){
+			$where = ' 1=1 ';
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'COD_UF', SqlHelper::SQL_TYPE_NUMERIC);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'NOM_UF', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'SIG_UF', SqlHelper::SQL_TYPE_TEXT_LIKE);
+			$where = SqlHelper::getAtributeWhereGridParameters($where, $whereGrid, 'COD_REGIAO', SqlHelper::SQL_TYPE_NUMERIC);
+			$result = $where;
+		}
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectById( $id ) {
+		if( empty($id) || !is_numeric($id) ){
+			throw new InvalidArgumentException();
+		}
+		$values = array($id);
+		$sql = self::$sqlBasicSelect.' where cod_uf = ?';
+		$result = self::executeSql($sql, $values );
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectCount( $where=null ){
+		$where = self::processWhereGridParameters($where);
+		$sql = 'select count(cod_uf) as qtd from form_exemplo.uf';
+		$sql = $sql.( ($where)? ' where '.$where:'');
+		$result = self::executeSql($sql);
+		return $result['QTD'][0];
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectAllPagination( $orderBy=null, $where=null, $page=null,  $rowsPerPage= null ) {
+		$rowStart = PaginationSQLHelper::getRowStart($page,$rowsPerPage);
+		$where = self::processWhereGridParameters($where);
+
+		$sql = self::$sqlBasicSelect
+		.( ($where)? ' where '.$where:'')
+		.( ($orderBy) ? ' order by '.$orderBy:'')
+		.( ' LIMIT '.$rowStart.','.$rowsPerPage);
+
+		$result = self::executeSql($sql);
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function selectAll( $orderBy=null, $where=null ) {
+		$where = self::processWhereGridParameters($where);
+		$sql = self::$sqlBasicSelect
+		.( ($where)? ' where '.$where:'')
+		.( ($orderBy) ? ' order by '.$orderBy:'');
+
+		$result = self::executeSql($sql);
+		return $result;
+	}
+	//--------------------------------------------------------------------------------
+	public static function insert( UfVO $objVo ) {
+		$values = array(  $objVo->getNom_uf() 
+						, $objVo->getSig_uf() 
+						, $objVo->getCod_regiao() 
+						);
+		return self::executeSql('insert into form_exemplo.uf(
 								 nom_uf
 								,sig_uf
 								,cod_regiao
-								) values (?,?,?)', $values 
-        );
-    }
-    //--------------------------------------------------------------------------------
-    public static function update( UfVO $objVo )
-    {
-        $values = array( $objVo->getNom_uf()
-         ,$objVo->getSig_uf()
-         ,$objVo->getCod_regiao()
-         ,$objVo->getCod_uf() );
-        return self::executeSql(
-            'update uf set 
+								) values (?,?,?)', $values );
+	}
+	//--------------------------------------------------------------------------------
+	public static function update ( UfVO $objVo ) {
+		$values = array( $objVo->getNom_uf()
+						,$objVo->getSig_uf()
+						,$objVo->getCod_regiao()
+						,$objVo->getCod_uf() );
+		return self::executeSql('update form_exemplo.uf set 
 								 nom_uf = ?
 								,sig_uf = ?
 								,cod_regiao = ?
-								where cod_uf = ?', $values
-        );
-    }
-    //--------------------------------------------------------------------------------
-    public static function delete( $id )
-    {
-        $values = array($id);
-        return self::executeSql('delete from uf where cod_uf = ?', $values);
-    }
+								where cod_uf = ?',$values);
+	}
+	//--------------------------------------------------------------------------------
+	public static function delete( $id ){
+		$values = array($id);
+		return self::executeSql('delete from form_exemplo.uf where cod_uf = ?',$values);
+	}
 }
 ?>
