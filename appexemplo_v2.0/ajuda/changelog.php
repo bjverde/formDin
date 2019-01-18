95c3eb89b309a7bb7f78ff7677481e002470f470
Bjverde <bjverde@yahoo.com.br>
2019-01-18 01:15:01 -0200

    :abc: APP #87

--- appexemplo_v2.0/ajuda/changelog.php
+++ appexemplo_v2.0/ajuda/changelog.php
@@ -11,13 +11,17 @@ $msgSysNameVersion = SYSTEM_NAME.' - v'.SYSTEM_VERSION;
 </head>
 <body>
     <h2><?php echo($msgSysNameVersion); ?></h2>
-    <li>2018-11-21 versão 2.0.1</li>
+    <ul>
+    <li>2018-11-21 versão 2.0.1
         <ul>
             <li>Atualizado conforme o SysGen v0.9.0</li>
         </ul>
-    <li>2017-11-27 versão 2.0.0</li>
+    </li>
+    <li>2017-11-27 versão 2.0.0
         <ul>
             <li>Primeira versão</li>
         </ul>
+    </li>
+    </ul>
 </body>
 </html>
