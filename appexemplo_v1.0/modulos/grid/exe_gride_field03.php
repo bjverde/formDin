c82bab980886cebdb7ee1d9e4240af2f9ae8d374
Bjverde <bjverde@yahoo.com.br>
2019-02-04 03:25:39 -0200

    :memo: APP #112 addSelectColumn set a DEFAULT valeu

--- appexemplo_v1.0/modulos/grid/exe_gride_field03.php
+++ appexemplo_v1.0/modulos/grid/exe_gride_field03.php
@@ -9,18 +9,21 @@ $dados['NOME'][]  = 'Linha1';
 $dados['ATIVO'][] = 'S';
 $dados['GRUPO'][] = 'A';
 $dados['SIT_OPCOES'][] = '1=>Um,2=>Dois';
+$dados['FK_TYPE_SCREEN_REFERENCED'][] = null;
 
 $dados['ID'][]    = 2;
 $dados['NOME'][]  = 'Linha2';
 $dados['ATIVO'][] = 'S';
 $dados['GRUPO'][] = 'A';
 $dados['SIT_OPCOES'][] = '3=Tres,4=>Quatro';
+$dados['FK_TYPE_SCREEN_REFERENCED'][] = null;
 
 $dados['ID'][]    = 3;
 $dados['NOME'][]  = 'Linha3';
 $dados['ATIVO'][] = 'N';
 $dados['GRUPO'][] = 'C';
 $dados['SIT_OPCOES'][] = null;
+$dados['FK_TYPE_SCREEN_REFERENCED'][] = null;
 
 $html1 = '<b>ATENÇÃO</b>: addSelectColumn para funcionar corretamente precisa que a chave primaria no grid seja definida';
 $frm->addHtmlField('html1', $html1, null, null, null, null)->setCss('border', '1px solid #ffeb3b')->setCss('background-color', '#ffffcc')->setCss('margin-bottom', '10px');
@@ -37,8 +40,39 @@ $gride->addColumn('ID', 'id');
 $gride->addColumn('NOME', 'Nome', 100);
 $gride->addColumn('ATIVO', 'Ativo');
 $gride->addColumn('GRUPO', 'grupo');
-$gride->addSelectColumn('sit_opcoes', 'Opções', 'SIT_OPCOES', '1=Amarelo,2=Verde');
+$gride->addSelectColumn('sit_opcoes', 'Opções', 'SIT_OPCOES', '1=Amarelo,2=Verde',70);
 //$gride->addSelectColumn('sit_opcoes','Opções','SIT_OPCOES',null,null,null,null,null,null,null,'VAL_OPCAO');
+
+$listFkType = array(1=>'Select',2=>'Autocomplete',3=>'Search on-line');
+$gride->addSelectColumn('FK_TYPE_SCREEN_REFERENCED'
+                       , 'Valor Pre-Selecionado'
+                       , 'FK_TYPE_SCREEN_REFERENCED'  //ID no campo na origem dos dados no gride
+                       , $listFkType                  //Array de possível valores
+                       , 80                           //5: largura do campo em pixel
+                       , true                         //6: Somente Leitura
+                       , ''                           //7: Label do Primeiro elemento
+                       , 2                            //8: Valor do Primeiro elemento
+                       , null                         //9: 
+                       , null                         //10:
+                       , null                         //11:
+                       );
+$gride->addSelectColumn('FK2'
+    , 'Valor Pre-Selecionado 2'
+    , 'FK2'  //ID no campo na origem dos dados no gride
+    , $listFkType                  //Array de possível valores
+    , 80                           //5: largura do campo em pixel
+    , true                         //6: Somente Leitura
+    , null                           //7: Label do Primeiro elemento
+    , 2                            //8: Valor do Primeiro elemento
+    , null                         //9:
+    , null                         //10:
+    , null                         //11:
+    );
+
+//$selCol = $gride->getColumn('FK_TYPE_SCREEN_REFERENCED');   // recuperar o objeto coluna NOME_COMPRADOR
+//$selCol->setInitialValueField(0);
+//$gride->addSelectColumn('sit_opcoes2', 'Opções2', 'SIT_OPCOES', '1=Amarelo,2=Verde');
+
 $frm->addHtmlField('gride', $gride);
 
 $frm->setAction('POST PAGINA');
