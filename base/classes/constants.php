9fcca03f87e85e515cf179404db2c90ef013ef01
Bjverde <bjverde@yahoo.com.br>
2019-03-06 16:40:43 -0300

    :memo: BASE new pre-release v4.3.1-alpha

--- base/classes/constants.php
+++ base/classes/constants.php
@@ -57,7 +57,7 @@ define('EOL', "\n");
 define('TAB', chr(9));
 
 
-define('FORMDIN_VERSION', '4.3.00');
+define('FORMDIN_VERSION', '4.3.1-alpha');
 
 // --Data Base Management System
 define('DBMS_ACCESS', 'ACCESS');
