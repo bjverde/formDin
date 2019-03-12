4fe0c7b8e9057cac1fdab4a1ab7ddc2d563c29d5
Bjverde <bjverde@yahoo.com.br>
2019-03-11 23:41:41 -0300

    :memo: BASE new release v4.3.1

--- base/classes/constants.php
+++ base/classes/constants.php
@@ -57,7 +57,7 @@ define('EOL', "\n");
 define('TAB', chr(9));
 
 
-define('FORMDIN_VERSION', '4.3.1-alpha');
+define('FORMDIN_VERSION', '4.3.1');
 
 // --Data Base Management System
 define('DBMS_ACCESS', 'ACCESS');
