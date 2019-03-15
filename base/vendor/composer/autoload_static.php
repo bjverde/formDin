6a062b8565f57954add356a44f5dbf70308aa3ff
Bjverde <bjverde@yahoo.com.br>
2019-03-15 00:03:21 -0300

    :hammer: BASE #131 inclusão de todos os arquivos

--- base/vendor/composer/autoload_static.php
+++ base/vendor/composer/autoload_static.php
@@ -6,9 +6,101 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit23e5b0c0a3f57ff24ebfb23e5f3e0a65
 {
+    public static $files = array (
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Tuupola\\Middleware\\' => 19,
+            'Tuupola\\Http\\Factory\\' => 21,
+        ),
+        'S' => 
+        array (
+            'Slim\\' => 5,
+        ),
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+            'Psr\\Http\\Server\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Container\\' => 14,
+        ),
+        'I' => 
+        array (
+            'Interop\\Container\\' => 18,
+        ),
+        'F' => 
+        array (
+            'Firebase\\JWT\\' => 13,
+            'FastRoute\\' => 10,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Tuupola\\Middleware\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tuupola/callable-handler/src',
+            1 => __DIR__ . '/..' . '/tuupola/slim-basic-auth/src',
+            2 => __DIR__ . '/..' . '/tuupola/slim-jwt-auth/src',
+        ),
+        'Tuupola\\Http\\Factory\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tuupola/http-factory/src',
+        ),
+        'Slim\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/slim/slim/Slim',
+        ),
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
+        'Psr\\Http\\Server\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-server-handler/src',
+            1 => __DIR__ . '/..' . '/psr/http-server-middleware/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'Interop\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
+        ),
+        'Firebase\\JWT\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
+        ),
+        'FastRoute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nikic/fast-route/src',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'Pimple' => 
+            array (
+                0 => __DIR__ . '/..' . '/pimple/pimple/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit23e5b0c0a3f57ff24ebfb23e5f3e0a65::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit23e5b0c0a3f57ff24ebfb23e5f3e0a65::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit23e5b0c0a3f57ff24ebfb23e5f3e0a65::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
---
+++ base/vendor/tuupola/slim-basic-auth/src/HttpBasicAuthentication/AuthenticatorInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of Slim HTTP Basic Authentication middleware
+ *
+ * Copyright (c) 2013-2018 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/slim-basic-auth
+ *
+ */
+
+namespace Tuupola\Middleware\HttpBasicAuthentication;
+
+interface AuthenticatorInterface
+{
+    public function __invoke(array $arguments): bool;
+}
