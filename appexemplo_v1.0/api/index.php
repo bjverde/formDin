0991b60c06c1104d333afd4fea6b68b4421892b0
Bjverde <bjverde@yahoo.com.br>
2019-03-15 00:58:07 -0300

    :hammer: APP #131 fazendo o exemplo

---
+++ appexemplo_v1.0/api/index.php
@@ -0,0 +1,11 @@
+<?php
+
+if (! defined ( 'DS' )) {
+	define ( 'DS', DIRECTORY_SEPARATOR );
+}
+$current_dirApi = dirname ( __FILE__ );
+
+
+require_once $current_dirApi.DS.'..'.DS.'..'.DS.'base/classes/webform/TApplication.class.php';
+require_once $current_dirApi.DS.'slimConfiguration.php';
+require_once $current_dirApi.DS.'routes.php';
---
+++ appexemplo_v1.0/api/routes.php
@@ -0,0 +1,43 @@
+<?php
+/*
+use function src\{
+    slimConfiguration,
+    basicAuth,
+    jwtAuth
+};
+use App\Controllers\{
+    ProdutoController,
+    LojaController,
+    AuthController
+};
+*/
+
+use Tuupola\Middleware\JwtAuthentication;
+//use App\Middlewares\JwtDateTimeMiddleware;
+
+$app = new \Slim\App(slimConfiguration());
+
+// =========================================
+
+$app->post('/login', AuthController::class . ':login');
+$app->post('/refresh-token', AuthController::class . ':refreshToken');
+
+$app->get('/teste', function() { echo "oi"; })
+    ->add(new JwtDateTimeMiddleware())
+    ->add(jwtAuth());
+
+$app->group('', function() use ($app) {
+    $app->get('/loja', LojaController::class . ':getLojas');
+    $app->post('/loja', LojaController::class . ':insertLoja');
+    $app->put('/loja', LojaController::class . ':updateLoja');
+    $app->delete('/loja', LojaController::class . ':deleteLoja');
+
+    $app->get('/produto', ProdutoController::class . ':getProdutos');
+    $app->post('/produto', ProdutoController::class . ':insertProduto');
+    $app->put('/produto', ProdutoController::class . ':updateProduto');
+    $app->delete('/produto', ProdutoController::class . ':deleteProduto');
+})->add(basicAuth());
+
+// =========================================
+
+$app->run();
