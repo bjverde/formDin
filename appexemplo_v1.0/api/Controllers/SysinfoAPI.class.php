da08224dfdd8e4c10aa72f3a9e0eec2b9f51e5fc
Bjverde <bjverde@yahoo.com.br>
2019-03-18 23:30:07 -0300

    melhorando exemplo

--- appexemplo_v1.0/api/Controllers/SysinfoAPI.class.php
+++ appexemplo_v1.0/api/Controllers/SysinfoAPI.class.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controllers;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 class SysinfoAPI {
     
     
@@ -8,12 +11,15 @@ class SysinfoAPI {
     }
     
     //--------------------------------------------------------------------------------
-    public static function getInfo(){
+    public static function getInfo(Request $request, Response $response, array $args): Response
+    {
         $result = array(
             'SYSTEM_NAME'=>SYSTEM_NAME
             ,'SYSTEM_ACRONYM'=>SYSTEM_ACRONYM
             ,'SYSTEM_VERSION'=>SYSTEM_VERSION
         );
-        return $result;
+        
+        $response = $response->withJson($result);
+        return $response;
     }
 }
--- appexemplo_v1.0/api/index.php
+++ appexemplo_v1.0/api/index.php
@@ -10,5 +10,12 @@ require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'constantes.php';
 require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'config_conexao.php';
 require_once $current_dirApi.DS.'..'.DS.'..'.DS.'base/classes/webform/TApplication.class.php';
 //require_once $current_dirApi.DS.'..'.DS.'classes'.DS.'autoload_ap1v.php';
+//-------------------------------------------
+require_once $current_dirApi.DS.'..'.DS.'dao'.DS.'Tb_pedidoDAO.class.php';
+require_once $current_dirApi.DS.'..'.DS.'classes'.DS.'Tb_pedido.class.php';
+require_once $current_dirApi.DS.'Controllers'.DS.'Tb_pedidoAPI.class.php';
+require_once $current_dirApi.DS.'Controllers'.DS.'SysinfoAPI.class.php';
+//-------------------------------------------
+require_once $current_dirApi.DS.'env.php';
 require_once $current_dirApi.DS.'slimConfiguration.php';
 require_once $current_dirApi.DS.'routes.php';
