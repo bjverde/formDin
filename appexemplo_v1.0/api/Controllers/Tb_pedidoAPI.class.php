299d948052d7ddb15dd3e3e9943c1798327ad3ef
Bjverde <bjverde@yahoo.com.br>
2019-03-21 01:28:05 -0300

    :hammer: BASE nova arquivo

---
+++ appexemplo_v1.0/api/Controllers/Tb_pedidoAPI.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class Tb_pedidoAPI {
+    
+    
+    public function __construct(){
+    }
+    
+    //--------------------------------------------------------------------------------
+    public static function selectAll(Request $request, Response $response, array $args): Response
+    {
+        $result = Tb_pedido::selectAll();
+        
+        $response = $response->withJson($result);
+        return $response;
+    }
+}
