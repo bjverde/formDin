ab53656cb335c0d4b2c01ad58bc24f997068d63b
Bjverde <bjverde@yahoo.com.br>
2019-04-14 21:06:49 -0300

    :memo: APP #148 includes autoloads

--- appexemplo_v2.0/includes/constantes.php
+++ appexemplo_v2.0/includes/constantes.php
@@ -15,4 +15,10 @@ define('SYSTEM_ACRONYM' , 'APPEV2.0');
 define('SYSTEM_VERSION' , '2.0.1');
 define('APLICATIVO'     , SYSTEM_ACRONYM);
 define('DS'   , DIRECTORY_SEPARATOR);
+
+define('FORMDIN_VERSION_MIN_VERSION', '4.5.1-alpha');
+
+if (! defined ( 'DS' )) {
+    define('DS'   , DIRECTORY_SEPARATOR);
+}
 ?>
