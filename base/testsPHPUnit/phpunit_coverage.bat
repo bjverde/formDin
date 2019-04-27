cd3ed0e434272a61a72580cee114683688605a79
Bjverde <bjverde@yahoo.com.br>
2019-04-27 01:24:10 -0300

    :hammer: BASE #153 update bat

--- base/testsPHPUnit/phpunit_coverage.bat
+++ base/testsPHPUnit/phpunit_coverage.bat
@@ -1,16 +1,17 @@
 REM 4.8.23 Final
-D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\php\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
-
-
-REM 5.7.23 Final
-REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+REN D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 5.7.23 Final
-REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
+REM 5.7.23 Final
+REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 6.4.3 Final
-REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 7.2.4 Final
-REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-7.2.4.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+REN D:\wamp\bin\php\php7.1.26\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+
+REM 7.2.4 Final
+D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-8.1.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
