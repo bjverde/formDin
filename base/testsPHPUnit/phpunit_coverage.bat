c0000b6b3fc544c5cb471d7faf9e3d7d0872f9bc
Bjverde <bjverde@yahoo.com.br>
2019-04-29 22:29:38 -0300

    :bug: BASE #153

--- base/testsPHPUnit/phpunit_coverage.bat
+++ base/testsPHPUnit/phpunit_coverage.bat
@@ -11,7 +11,7 @@ REM 6.4.3 Final
 REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 7.2.4 Final
-D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base\ --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 8.1.3 Final
 REN D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-8.1.3.phar --whitelist D:\wamp\www\formDin\base\ --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
