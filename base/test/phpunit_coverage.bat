ba76f9a3766ecb72977c04fe218dea5b83809f63
Bjverde <bjverde@yahoo.com.br>
2019-06-22 00:26:09 -0300

    :bug: BASE #165 fix scripts

--- base/test/phpunit_coverage.bat
+++ base/test/phpunit_coverage.bat
@@ -1,5 +1,5 @@
 REM 4.8.23 Final
-REN D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
+REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 5.7.23 Final
 REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
@@ -14,7 +14,7 @@ ECHO 7.2.4 Coverage
 REM D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
 
 ECHO 7.2.4 Config XML and Coverage
-REM D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml --coverage-html D:\wamp\www\formDin5\phpunit-code-coverage D:\wamp\www\formDin\base\test\
+REM D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
 
 ECHO 7.2.4 Config XML file
 D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml D:\wamp\www\formDin\base\test\
