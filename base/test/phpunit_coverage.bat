eca384979bc6c4f807723887c480b82631823e79
Bjverde <bjverde@yahoo.com.br>
2019-06-26 03:33:18 -0300

    :abc: BASE #166

--- base/test/phpunit_coverage.bat
+++ base/test/phpunit_coverage.bat
@@ -4,17 +4,17 @@ REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-4.8.36.phar --
 REM 5.7.23 Final
 REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
-REM 5.7.23 Final
+REM ECHO PHP 7.1.16 and PHPUnit 5.7.23 Final
 REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
-REM 6.4.3 Final
+REM ECHO PHP 7.1.16 and PHPUnit 6.4.3 Final
 REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
 
-ECHO 7.2.4 Coverage
-REM D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
+REM ECHO PHP 7.2.18 and PHPUnit 7.2.4 with Coverage
+REM D:\wamp\bin\php\php7.2.18\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
 
-ECHO 7.2.4 Config XML and Coverage
-REM D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
+REM ECHO PHP 7.2.18 and PHPUnit 7.2.4 with Config XML and Coverage
+REM D:\wamp\bin\php\php7.2.18\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
 
-ECHO 7.2.4 Config XML file
-D:\wamp\bin\php\php7.2.14\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml D:\wamp\www\formDin\base\test\
+ECHO PHP 7.2.18 and PHPUnit 7.2.4 with Config XML file
+D:\wamp\bin\php\php7.2.18\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml D:\wamp\www\formDin\base\test\
