5144c5e11b296d19ab45d863b97cf92a5a98791a
Bjverde <bjverde@yahoo.com.br>
2019-07-26 02:09:20 -0300

    :memo: new pre-release v4.7.4-alpha

--- base/classes/constants.php
+++ base/classes/constants.php
@@ -66,7 +66,7 @@ if (!defined('TAB')) {
 }
 
 
-define('FORMDIN_VERSION', '4.7.3');
+define('FORMDIN_VERSION', '4.7.4-alpha');
 
 // --Data Base Management System
 define('DBMS_ACCESS', 'ACCESS');
--- base/test/classes/helpers/FormDinHelperTest.php
+++ base/test/classes/helpers/FormDinHelperTest.php
@@ -49,7 +49,7 @@ class FormDinHelperTest extends TestCase
 {
 
     public function testVersion() {
-		$expected = '4.7.3';
+		$expected = '4.7.4';
 		$result =  FormDinHelper::version();
 		$this->assertEquals( $expected , $result);
 	}
