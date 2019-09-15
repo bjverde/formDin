03ce41310889259f5f0f2d90dc8fa6408b392151
Bjverde <bjverde@yahoo.com.br>
2019-09-15 20:03:59 -0300

    :memo: :fire: APP #187

--- appexemplo_v2.0/modulos/acesso_aviso.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$html = '<b>Aviso</b>'
-       .'<br>Essa tela só funciona do Aplicativo de exemplo 2.5'
-       .'<br>No Aplicativo de exemplo 2.0 serve apenas mostrar';
-$frm->addHtmlField('texto', $html)->setCss('border', '1px solid red');;
-$frm->addHtmlField('linha', ' ');
