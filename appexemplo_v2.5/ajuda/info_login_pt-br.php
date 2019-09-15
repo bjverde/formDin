97003bcb0d44b06e0bd9db20e0633827de9043cf
Bjverde <bjverde@yahoo.com.br>
2019-09-15 18:43:08 -0300

    :memo: APP #188

---
+++ appexemplo_v2.5/ajuda/info_login_pt-br.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+	</head>
+	<body>
+       <br><br>Existem 4 usuários, cada um com um perfil diferete!
+       <br>
+       <ul>
+           <li>login: root e senha: root &rarr; é o super usuário, que pode criar novas telas com o gerador </li>
+           <li>login: admin e senha: admin &rarr; é o super administrador, pode incluir outros usuarios e conceder acesso </li>
+           <li>login: user e senha: user &rarr; usario comum pode operar todas as telas</li>
+           <li>login: trainee e senha: trainee &rarr; tem acesso apenas uma tela</li>
+       <ul>        
+	</body>
+</html>
