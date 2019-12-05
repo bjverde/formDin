28568c06abec556ef99414738043e149fea02340
Bjverde <bjverde@yahoo.com.br>
2019-12-05 00:56:34 -0300

    :memo: new release v4.8.0

--- base/test/classes/helpers/FormDinHelperTest.php
+++ base/test/classes/helpers/FormDinHelperTest.php
@@ -51,7 +51,7 @@ class FormDinHelperTest extends TestCase
 {
 
     public function testVersion() {
-		$expected = '4.7.9-alpha';
+		$expected = '4.8.0';
 		$result =  FormDinHelper::version();
 		$this->assertEquals( $expected , $result);
 	}
