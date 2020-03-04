677121321ba9164af100c63ae03eadee9075149e
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2020-03-04 18:17:11 -0300

    :memo: #129 criando autenticação do REST Slim

--- appexemplo_v2.0/api/index.php
+++ appexemplo_v2.0/api/index.php
@@ -10,12 +10,16 @@
  * System appev2 created in: 2019-09-10 09:04:46
  */
 
+ 
 if (! defined ( 'DS' )) {
     define ( 'DS', DIRECTORY_SEPARATOR );
 }
 $current_dirApi = dirname ( __FILE__ );
 
-
+/***
+  * Baseado no curso APIs REST com PHP 7 e Slim Framework
+  * https://github.com/codeeasy-dev/apis-rest-com-php-7-e-slim-framework
+  */
 require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'constantes.php';
 require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'config_conexao.php';
 require_once $current_dirApi.DS.'..'.DS.'..'.DS.'base/classes/webform/TApplication.class.php';
@@ -23,7 +27,7 @@ require_once $current_dirApi.DS.'..'.DS.'controllers'.DS.'autoload_appev2.php';
 require_once $current_dirApi.DS.'..'.DS.'dao'.DS.'autoload_appev2_dao.php';
 require_once $current_dirApi.DS.'autoload_appev2_api.php';
 
-    //--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
 require_once $current_dirApi.DS.'env.php';
 require_once $current_dirApi.DS.'slimConfiguration.php';
 require_once $current_dirApi.DS.'routes.php';
