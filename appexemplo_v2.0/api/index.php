c720aafabcfd0186bacc1e8e94ddb1aff7ac6c6f
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2020-03-04 18:21:56 -0300

    :memo: #129 incluido autenticação

--- appexemplo_v2.0/api/index.php
+++ appexemplo_v2.0/api/index.php
@@ -29,5 +29,7 @@ require_once $current_dirApi.DS.'autoload_appev2_api.php';
 
 //--------------------------------------------------------------------------------
 require_once $current_dirApi.DS.'env.php';
+require_once $current_dirApi.DS.'basicAuth.php';
+require_once $current_dirApi.DS.'jwtAuth.php';
 require_once $current_dirApi.DS.'slimConfiguration.php';
 require_once $current_dirApi.DS.'routes.php';
