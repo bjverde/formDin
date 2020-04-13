9d6a8fc2571f9c3de83a24cf13b1fae8f49db522
Bjverde <bjverde@yahoo.com.br>
2020-04-12 21:45:01 -0300

    :hammer: BASE novas funcoes

--- base/classes/helpers/StringHelper.class.php
+++ base/classes/helpers/StringHelper.class.php
@@ -78,4 +78,29 @@ class StringHelper
         return $string;
     }
     
+
+    /**
+     * Recebe uma string e formata com CPF ou CNPJ
+     * https://gist.github.com/davidalves1/3c98ef866bad4aba3987e7671e404c1e
+     * @param string $value
+     * @return string
+     */
+    public static function formatCnpjCpf($value) 
+    {
+        $cnpj_cpf = preg_replace("/\D/", '', $value);
+        if (strlen($cnpj_cpf) === 11) {
+            $value = preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+        } else if(strlen($cnpj_cpf) === 14){
+            $value = preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
+        }
+        return $value;
+    }
+
+    public static function limpaCnpjCpf($value) 
+    {
+        $limpo = preg_replace("/\D/", '', $value);
+        return $limpo;
+    }
+
+
 }
