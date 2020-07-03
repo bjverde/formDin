9720459d466b8271181b25f5a3e7fa6a7cf0e459
bjverde <bjerde@yahoo.com.br>
2020-07-03 16:19:52 -0300

    :hammer: BASE FIXED #227

--- base/classes/helpers/FormDinHelper.class.php
+++ base/classes/helpers/FormDinHelper.class.php
@@ -179,5 +179,67 @@ class FormDinHelper
     }
     
 
+    /***
+     * função para depuração. Exibe o modulo a linha e a variável/objeto solicitado
+     * Retirado do FormDin 4.9.0
+     * https://github.com/bjverde/formDin/blob/master/base/includes/funcoes.inc
+     */
+    public static function debug( $mixExpression,$strComentario='Debug', $boolExit=FALSE, $showBackTrace=false ) {
+        ini_set ( 'xdebug.max_nesting_level', 150 );
+        if (defined('DEBUGAR') && !DEBUGAR){
+            return;
+        }
+        $arrBacktrace = debug_backtrace();
+        if( isset($_REQUEST['ajax']) && $_REQUEST['ajax'] ){
+            echo '<div class="formDinDebug">';
+            echo '<pre>';
+            foreach ( $arrBacktrace[0] as $strAttribute => $mixValue ){
+                if ( !is_array($mixValue) ){
+                    echo $strAttribute .'='. $mixValue ."\n";
+                }
+            }
+            echo "---------------\n";
+            print_r( $mixExpression );
+            echo '</pre>';
+            echo '</div>';
+        } else {
+            echo '<div class="formDinDebug">';
+            echo "<script>try{fwUnblockUI();}catch(e){try{top.app_unblockUI();}catch(e){}}</script>";
+            echo "<fieldset style='text-align:left;'><legend><font color=\"#007000\">".$strComentario."</font></legend><pre>" ;
+            foreach ( $arrBacktrace[0] as $strAttribute => $mixValue ) {
+                if( !is_array($mixValue) ) {
+                    echo "<b>" . $strAttribute . "</b> ". $mixValue ."\n";
+                }
+            }
+            echo "</pre><hr />";
+            echo '<span style="color:red;"><blink>'.$strComentario.'</blink></span>'."\n";;
+            echo '<pre>';
+            if( is_object($mixExpression) ) {
+                var_dump( $mixExpression );
+            } else {
+                print_r($mixExpression);
+            }
+            echo '</pre>';
+            
+            if($showBackTrace==true){
+                echo '<hr>';
+                echo '<font style="color:red;">BackTrack</font>';
+                echo '<pre>';
+                foreach ( $arrBacktrace as $key => $value ){
+                    echo $value['file'] .' <b>line:</b> '.$value['line'].' <b>function:</b> '.$value['function']."\n";
+                }
+                echo '</pre>';
+            }
+            
+            echo '</fieldset>';
+            echo '</div>';
+            if ( $boolExit ) {
+                echo "<br /><font color=\"#700000\" size=\"4\"><b>D I E</b></font>";
+                exit();
+            }
+        }
+    }
+    
+    
 }
 ?>
