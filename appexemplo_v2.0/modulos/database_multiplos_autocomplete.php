9d9b52b9888f21d00b5d7ef3200d9df4e3e8159e
bjverde <bjverde@yahoo.com.br>
2020-10-26 02:39:36 -0300

    :memo: APP #230

--- appexemplo_v2.0/modulos/database_multiplos_autocomplete.php
+++ appexemplo_v2.0/modulos/database_multiplos_autocomplete.php
@@ -2,21 +2,35 @@
 defined('APLICATIVO') or die();
 require_once 'modulos/includes/acesso_view_allowed.php';
 
-$html1 = '<h1>Exemplo acessando diversos bancos</h1>';
+$html1 = '<h1>Exemplo setAutoComplete com diversos bancos</h1>';
 $html1 = $html1.'<br>'; 
-$html1 = $html1.'Ao Clicar executar 3 ações de banco serão executadas';
-$html1 = $html1.'<br> 1 - Select em tabela MySql';
+$html1 = $html1.'<br> No grupo Empresa - está conectado o banco MySql, na tebela pessoa. Pesquisando nomes de Empresas recondo usar BRAS';
 $html1 = $html1.'<br> 2 - Select em tabela SqLite';
 $html1 = $html1.'<br> 3 - Select em tabela MySql';
 $html1 = $html1.'<br>';
-$html1 = $html1.'<br> Reparece que o banco MySql é conexão principal, portanto não precisa ser informada de forma EXPLICITA';
 
 $frm = new TForm('Exemplo Multiplos');
 $frm->setFlat(true);
 $frm->setMaximize(true);
 $frm->addHtmlField('html1', $html1, null, null, null, null)->setClass('boxAlert',true);
 
-$frm->addButton('Executar', null, 'Executar', null, null, true, false)->setClass('btnOragen', false);
+$frm->addGroupField('gpx1','Empresa');
+    $frm->addHiddenField('TIPO','PJ');
+    $frm->addNumberField('IDPESSOA', 'Cod',4,true,0);
+    $frm->addTextField('NOM_PESSOA', 'Nome',150,true,70,null,false);
+    //Deve sempre ficar depois da definição dos campos
+    $frm->setAutoComplete('NOM_PESSOA'
+        ,'pessoa'// tabela
+        ,'NOME'	 		// campo de pesquisa
+        ,'IDPESSOA|IDPESSOA,NOME|NOM_PESSOA' // campo que será atualizado ao selecionar o nome do município <campo_tabela> | <campo_formulario>
+        ,true
+        ,'TIPO' 		    // campo do formulário que será adicionado como filtro
+        ,null				// função javascript
+        ,3					// Default 3, numero de caracteres minimos para disparar a pesquisa
+        ,500				// 9: Default 1000, tempo após a digitação para disparar a consulta
+        ,50					//10: máximo de registros que deverá ser retornado
+        , null, null, null, null, true, null, null, true );
+$frm->closeGroup();
 
 
 $acao = isset($acao) ? $acao : null;
