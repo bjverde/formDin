b38e760e3faa27859b3810e85e51bcefcc6b54d0
bjverde <bjverde@yahoo.com.br>
2020-10-26 03:16:44 -0300

    :memo: APP #230

--- appexemplo_v2.0/modulos/database_multiplos_autocomplete.php
+++ appexemplo_v2.0/modulos/database_multiplos_autocomplete.php
@@ -4,9 +4,8 @@ require_once 'modulos/includes/acesso_view_allowed.php';
 
 $html1 = '<h1>Exemplo setAutoComplete com diversos bancos</h1>';
 $html1 = $html1.'<br>'; 
-$html1 = $html1.'<br> No grupo Empresa - está conectado o banco MySql, na tebela pessoa. Pesquisando nomes de Empresas recondo usar BRAS';
-$html1 = $html1.'<br> 2 - Select em tabela SqLite';
-$html1 = $html1.'<br> 3 - Select em tabela MySql';
+$html1 = $html1.'<br> No grupo "MySql: Empresa" - está conectado o banco MySql, na tebela pessoa. Pesquisando nomes de Empresas recondo usar BRAS';
+$html1 = $html1.'<br> No grupo "SqLite: Municípios" - está conectado o banco SqLite, na tebela tb_municipio. Pesquisando nomes de Municípios recondo usar SERRA';
 $html1 = $html1.'<br>';
 
 $frm = new TForm('Exemplo Multiplos');
@@ -14,7 +13,7 @@ $frm->setFlat(true);
 $frm->setMaximize(true);
 $frm->addHtmlField('html1', $html1, null, null, null, null)->setClass('boxAlert',true);
 
-$frm->addGroupField('gpx1','Empresa');
+$frm->addGroupField('gpx1','MySql: Empresa');
     $frm->addHiddenField('TIPO','PJ');
     $frm->addNumberField('IDPESSOA', 'Cod',4,true,0);
     $frm->addTextField('NOM_PESSOA', 'Nome',150,true,70,null,false);
@@ -30,6 +29,31 @@ $frm->addGroupField('gpx1','Empresa');
         ,500				// 9: Default 1000, tempo após a digitação para disparar a consulta
         ,50					//10: máximo de registros que deverá ser retornado
         , null, null, null, null, true, null, null, true );
+$frm->closeGroup();
+
+
+$frm->addGroupField('gpx2', 'SqLite: Municípios');
+    $frm->addTextField('nom_municipio2', 'Nome:', 60)->setExampleText('Digite os 3 primeiros caracteres.')->addEvent('onblur', 'validarMunicipio(this)');
+    $frm->addTextField('cod_uf2', 'Cód Uf 2:', false);
+
+    $frm->setAutoComplete('nom_municipio2'
+                        , 'tb_municipio'            // 2: tabela alvo da pesquisa
+                        , 'nom_municipio'           // 3: campo de pesquisa
+                        , 'cod_municipio|cod_municipio2,cod_uf|cod_uf2'  // 4: campos do form origem que serão atualizados ao selecionar o item desejado. Separados por virgulas seguindo o padrão <campo_tabela> | <campo_formulario> , <campo_tabela> | <campo_formulario>
+                        , true
+                        , null                      // 6: campo do formulário que será adicionado como filtro
+                        , null
+                        , 3
+                        , 1000                       // 09: Default 1000, tempo após a digitação para disparar a consulta
+                        , 50                         // 10: máximo de registros que deverá ser retornado
+                        , null, null, null           // url da função de callbacks, se ficar em branco será tratado por callbacks/autocomplete.php
+                        , null                       // 15: Mesagem caso não encontre nenhum registro
+                        , null
+                        , null
+                        , null
+                        , true                       // 18: $boolSearchAnyPosition busca o texto em qualquer posição igual Like %texto%
+                        , 'config_sqlite.php'
+                    );      
 $frm->closeGroup();
 
 
