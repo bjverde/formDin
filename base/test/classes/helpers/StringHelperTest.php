23b0ed09e7bdba6d0bc445f1cccede052bfa708d
Bjverde <bjverde@yahoo.com.br>
2022-02-13 17:06:45 -0300

    :white_check_mark: #247 BASE add new tests

--- base/test/classes/helpers/StringHelperTest.php
+++ base/test/classes/helpers/StringHelperTest.php
@@ -51,6 +51,30 @@ use PHPUnit\Framework\TestCase;
 class StringHelperTest extends TestCase
 {
     const STRING_ORIGIN = 'Você deve ter recebido uma cópia da GNU LGPL versão 3';
+
+    public function testStr2Lower_nochange() {
+        $expected = 'você deve ter recebido uma cópia da gnu lgpl versão 3';
+		$result = StringHelper::strtolower_utf8($expected) ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testStr2Lower_change() {
+        $expected = 'você deve ter recebido uma cópia da gnu lgpl versão 3';
+		$result = StringHelper::strtolower_utf8(self::STRING_ORIGIN) ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testStr2Upper_nochange() {
+        $expected = 'VOCÊ DEVE TER RECEBIDO UMA CÓPIA DA GNU LGPL VERSÃO 3';
+		$result = StringHelper::strtoupper_utf8($expected) ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testStr2Upper_change() {
+        $expected = 'VOCÊ DEVE TER RECEBIDO UMA CÓPIA DA GNU LGPL VERSÃO 3';
+		$result = StringHelper::strtoupper_utf8(self::STRING_ORIGIN) ;		
+		$this->assertEquals( $expected , $result );
+	}	
 	
     public function testStr2utf8_notUtf8ISO88591() {
         $str = self::STRING_ORIGIN;
@@ -92,5 +116,89 @@ class StringHelperTest extends TestCase
 	    
 	    $this->assertEquals( self::STRING_ORIGIN ,$str);
 	    $this->assertEquals( 'UTF-8' , $result);
+	}
+
+	public function testFormatCnpjCpf_forapadrao() {
+        $expected = '123.456.789-09';
+		$result = StringHelper::formatCnpjCpf('abc 123.456.789-09 flajk') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatCnpjCpf_cpf() {
+        $expected = '123.456.789-09';
+		$result = StringHelper::formatCnpjCpf('12345678909') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatCnpjCpf_cnpj() {
+        $expected = '09.344.726/0001-16';
+		$result = StringHelper::formatCnpjCpf('09344726000116') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testLimpaCnpjCpf() {
+        $expected = '12345678909';
+		$result = StringHelper::limpaCnpjCpf('abc 123.456.789-09 flajk') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatPhoneNumber_celularComDdd() {
+        $expected = '(61) 99982-2045';
+		$result = StringHelper::formatPhoneNumber('61999822045') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatPhoneNumber_fixoComDdd() {
+        $expected = '(61) 1234-5678';
+		$result = StringHelper::formatPhoneNumber('611234-5678') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatPhoneNumber_celularSemDdd() {
+        $expected = '99982-2045';
+		$result = StringHelper::formatPhoneNumber('999822045') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testFormatPhoneNumber_fixoSemDdd() {
+        $expected = '1234-5678';
+		$result = StringHelper::formatPhoneNumber('12345678') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+    public function testTirarAcentos() {
+        $expected = 'Voce deve ter recebido uma copia da GNU LGPL versao 3';
+		$result = StringHelper::tirarAcentos(self::STRING_ORIGIN) ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testTirarAcentos2() {
+        $expected = 'acao ACAO Nao nAo';
+		$result = StringHelper::tirarAcentos('ação AÇÃO Não nÃo') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testString2PascalCase() {
+        $expected = 'AcaoDeletarMao';
+		$result = StringHelper::string2PascalCase('ação deLEtar MÃO') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testString2CamelCase() {
+        $expected = 'acaoDeletarMao';
+		$result = StringHelper::string2CamelCase('ação deLEtar MÃO') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testString2KebabCase() {
+        $expected = 'acao-deletar-mao';
+		$result = StringHelper::string2KebabCase('ação deLEtar MÃO') ;		
+		$this->assertEquals( $expected , $result );
+	}
+
+	public function testString2SnakeCase() {
+        $expected = 'acao_deletar_mao';
+		$result = StringHelper::string2SnakeCase('ação deLEtar MÃO') ;		
+		$this->assertEquals( $expected , $result );
 	}
 }
