b32660eca50e5acaacadb7fc035a5623a051147e
Bjverde <bjverde@yahoo.com.br>
2022-02-22 21:44:24 -0300

    :bug: BASE #247 retro compatibilidade

--- base/vendor/composer/platform_check.php
+++ base/vendor/composer/platform_check.php
@@ -4,7 +4,7 @@
 
 $issues = array();
 
-if (!(PHP_VERSION_ID >= 80000)) {
+if (!(PHP_VERSION_ID >= 70300)) {
     $issues[] = 'Your Composer dependencies require a PHP version ">= 8.0.0". You are running ' . PHP_VERSION  .  '.';
 }
 
