96124bcb8818e966dfef9f0aa2484c01ae1f7f8d
Bjverde <bjverde@yahoo.com.br>
2022-02-22 22:51:36 -0300

    :bug: BASE #247 Deprecated: strtoupper Passing null

--- base/classes/webform/TForm.class.php
+++ base/classes/webform/TForm.class.php
@@ -2258,9 +2258,9 @@ class TForm Extends TBox
             {
                 return $this->displayControls[ StringHelper::strtolower($strFieldName) ]->getField();
             }
-            else if( array_key_exists( strtoupper($strFieldName), $this->displayControls ) )
+            else if( array_key_exists( StringHelper::strtoupper($strFieldName), $this->displayControls ) )
             {
-                return $this->displayControls[ strtoupper($strFieldName) ]->getField();
+                return $this->displayControls[ StringHelper::strtoupper($strFieldName) ]->getField();
             }
             // varrer todo o array para pesquisar dentro de abas e grupos
             foreach( $this->displayControls as $id=>$dc )
