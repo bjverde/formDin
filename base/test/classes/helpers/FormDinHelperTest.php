9594506e78ae32ed3c59dad46d5397ce40cc900f
Bjverde <bjverde@yahoo.com.br>
2022-03-10 18:59:44 -0300

    :bug: BASE #217 fix tests

--- base/test/classes/helpers/FormDinHelperTest.php
+++ base/test/classes/helpers/FormDinHelperTest.php
@@ -51,7 +51,7 @@ class FormDinHelperTest extends TestCase
 {
 
     public function testVersion() {
-		$expected = '4.14.1';
+		$expected = '4.15.0';
 		$result =  FormDinHelper::version();
 		$this->assertEquals( $expected , $result);
 	}
