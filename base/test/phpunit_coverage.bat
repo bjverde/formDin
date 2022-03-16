88d390dca8db54ce310b382805199506bfc839ff
Bjverde <bjverde@yahoo.com.br>
2022-03-16 13:04:23 -0300

    :hammer: #250 update testes

--- base/test/phpunit_coverage.bat
+++ base/test/phpunit_coverage.bat
@@ -1,21 +1,3 @@
-REM 4.8.23 Final
-REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
-
-REM 5.7.23 Final
-REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
-
-REM ECHO PHP 7.1.16 and PHPUnit 5.7.23 Final
-REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
-
-REM ECHO PHP 7.1.16 and PHPUnit 6.4.3 Final
-REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\testsPHPUnit\
-
-REM ECHO PHP 7.2.18 and PHPUnit 7.2.4 with Coverage
-REM D:\wamp\bin\php\php7.2.18\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --whitelist D:\wamp\www\formDin\base --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
-
-REM ECHO PHP 7.2.18 and PHPUnit 7.2.4 with Config XML and Coverage
-REM D:\wamp\bin\php\php7.2.18\php.exe D:\wamp\bin\phpunit\phpunit-7.2.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml --coverage-html D:\wamp\www\formDin\phpunit-code-coverage D:\wamp\www\formDin\base\test\
-
 REM ECHO php 7.3.5 and PHPUnit 9.1.4 with Config XML file
 REM D:\wamp\bin\php\php7.3.5\php.exe D:\wamp\bin\phpunit\phpunit-9.1.4.phar --configuration D:\wamp\www\formDin\base\test\phpunit-conf-win.xml D:\wamp\www\formDin\base\test\
 
