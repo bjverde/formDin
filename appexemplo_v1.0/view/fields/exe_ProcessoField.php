e5cc5add633ddddb0421c586aa5e49c6fd4b98e9
raphael.cavalcante <raphael.cavalcante@mpdft.mp.br>
2022-05-11 17:33:43 -0300

    [#254] Adicionando o form de teste no app 1.0

---
+++ appexemplo_v1.0/view/fields/exe_ProcessoField.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * Formdin Framework
+ * Copyright (C) 2012 Ministério do Planejamento
+ * Criado por Luís Eugênio Barbosa
+ * Essa versão é um Fork https://github.com/bjverde/formDin
+ *
+ * ----------------------------------------------------------------------------
+ * This file is part of Formdin Framework.
+ *
+ * Formdin Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Formdin.
+ *
+ * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuí1do na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licen?a Pública Geral GNU/LGPL em portugu?s
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+
+$frm = new TForm('Campos de processos');
+
+
+$frm->addProcessoField('processo','Campo de processo federal: ',false,true)->setExampleText('Ex: 02000.000343/2020-51');
+$frm->addnumeroDistribuicaoTJDFTField('numeroDistTJDFT','Campo do número único do TJDFT: ',false,true)->setExampleText('Ex: 0700444-79.2021.8.07.0003');
+
+$frm->addNumberField('num_peso', 'Peso Unitário:', 5, true, 2, true)->setExampleText('Kg');
+$frm->addNumberField('num_preco', 'Preço Unitário:', 9, true, 2, false)->setExampleText('R$');
+$frm->set('num_peso', '2.9');
+$frm->setAction('Gravar,Testar');
+
+$frm->set('processo', '02000.000343/2020-51');
+
+#####.######/##-##
+// numero existe $frm->set('processoMPDFT', '08191.085650/2021-61');
+// numero unico 0700444-79.2021.8.07.0003
+
+$acao = isset($acao) ? $acao : null;
+if ($acao=='Gravar') {
+    if ($frm->validate()) {
+        //d($_POST);
+        $bvars = $frm->createBvars('num_pessoa,val_salario,val_taxa');
+        //d($bvars);
+        $frm->setPopUpMessage('Validação OK');
+    }
+}
+// exibir o formulário
+$frm->show();
