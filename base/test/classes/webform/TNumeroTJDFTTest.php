68bda2afebb022bfb4f9a38c5b4930258bded1df
Bjverde <bjverde@yahoo.com.br>
2022-05-19 22:48:10 -0300

    :white_check_mark: BASE #253 novos testes

---
+++ base/test/classes/webform/TNumeroTJDFTTest.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * Formdin Framework
+ * Copyright (C) 2012 Ministério do Planejamento
+ * Criado por Luís Eugênio Barbosa
+ * Essa versão é um Fork https://github.com/bjverde/formDin
+ *
+ * ----------------------------------------------------------------------------
+ * This file is part of Formdin Framework.
+ *
+ * Formdin Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Formdin.
+ *
+ * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+$path =  __DIR__.'/../../../classes/';
+require_once $path.'constants.php';
+require_once $path.'webform/autoload_formdin.php';
+require_once $path.'helpers/autoload_formdin_helper.php';
+
+use PHPUnit\Framework\TestCase;
+
+
+class TNumeroTJDFTTest extends TestCase
+{
+    
+   
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp(): void {
+		parent::setUp ();
+	}
+	
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown(): void {
+		parent::tearDown ();
+	}
+	
+	public function testDistribuicao_apenas() {
+	    $test = new TNumeroTJDFT('disrib', 'NumeroDistribuicao',false,true,false);
+	    $result = $test->getMaxLenght();
+	    $this->assertSame(20, $result);
+        $result = $test->getSize();
+	    $this->assertSame(14, $result);
+	}
+	public function testNumeroUnico_apenas() {
+	    $test = new TNumeroTJDFT('unico', 'NumeroUnico',false,false,true);
+	    $result = $test->getMaxLenght();
+	    $this->assertSame(26, $result);
+        $result = $test->getSize();
+	    $this->assertSame(20, $result);
+	}
+	public function testNumeroTJDFT_geral() {
+	    $test = new TNumeroTJDFT('unico', 'NumeroUnico',false);
+	    $result = $test->getMaxLenght();
+	    $this->assertSame(26, $result);
+        $result = $test->getSize();
+	    $this->assertSame(20, $result);
+	}    
+
+}
