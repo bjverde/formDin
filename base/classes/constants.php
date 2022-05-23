07fdcb41cb72bba0ef8e7b3169929694464973bb
raphael.cavalcante <raphael.cavalcante@mpdft.mp.br>
2022-05-23 19:36:29 -0300

    BASE #261 Versão do sistema

--- base/classes/constants.php
+++ base/classes/constants.php
@@ -66,7 +66,7 @@ if (!defined('TAB')) {
 }
 
 
-define('FORMDIN_VERSION', '4.16.3');
+define('FORMDIN_VERSION', '4.17.0');
 
 // --Data Base Management System
 define('DBMS_ACCESS', 'ACCESS');
--- base/test/classes/helpers/FormDinHelperTest.php
+++ base/test/classes/helpers/FormDinHelperTest.php
@@ -50,7 +50,7 @@ use PHPUnit\Framework\TestCase;
 class FormDinHelperTest extends TestCase
 {
     public function testVersion() {
-		$expected = '4.16.3' ;
+		$expected = '4.17.0' ;
 		$result =  FormDinHelper::version();
 		$this->assertEquals( $expected , $result);
 	}
