e7d515251dd83a4d4f673fe293cc551f6c29ab38
Bjverde <bjverde@yahoo.com.br>
2022-09-23 22:46:52 -0300

    :abc: APP #270 formatando codigo

--- appexemplo_v2.0/api/index.php
+++ appexemplo_v2.0/api/index.php
@@ -15,11 +15,6 @@ if (! defined ( 'DS' )) {
     define ( 'DS', DIRECTORY_SEPARATOR );
 }
 $current_dirApi = dirname ( __FILE__ );
-
-/***
-  * Baseado no curso APIs REST com PHP 7 e Slim Framework
-  * https://github.com/codeeasy-dev/apis-rest-com-php-7-e-slim-framework
-  */
 require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'constantes.php';
 require_once $current_dirApi.DS.'..'.DS.'includes'.DS.'config_conexao.php';
 require_once $current_dirApi.DS.'..'.DS.'..'.DS.'base/classes/webform/TApplication.class.php';
@@ -28,6 +23,10 @@ require_once $current_dirApi.DS.'..'.DS.'dao'.DS.'autoload_appev2_dao.php';
 require_once $current_dirApi.DS.'autoload_appev2_api.php';
 
 //--------------------------------------------------------------------------------
+/***
+  * Baseado no curso APIs REST com PHP 7 e Slim Framework
+  * https://github.com/codeeasy-dev/apis-rest-com-php-7-e-slim-framework
+  */
 require_once $current_dirApi.DS.'env.php';
 //require_once $current_dirApi.DS.'basicAuth.php';
 //require_once $current_dirApi.DS.'jwtAuth.php';
