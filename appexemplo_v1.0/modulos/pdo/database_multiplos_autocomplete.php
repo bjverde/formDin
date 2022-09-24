202ac6f42a49f1b4c3e643daceb4762903c77732
Bjverde <bjverde@yahoo.com.br>
2022-09-24 18:10:41 -0300

    :hammer:: APP #230 novo exemplo

---
+++ appexemplo_v1.0/modulos/pdo/database_multiplos_autocomplete.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Formdin Framework
+ * Copyright (C) 2012 Ministério do Planejamento
+ * Criado por Luís Eugênio Barbosa
+ * Essa versão é um Fork https://github.com/bjverde/formDin
+ *
+ * ----------------------------------------------------------------------------
+ * This file is part of Formdin Framework.
+ *
+ * Formdin Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Formdin.
+ *
+ * O Framework Formdin é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+
+
+$html1  = 'o FormDin faz uso da classe TPDOConnectionObj.class.php para a conexão de banco de dados'
+.'<br>'
+.'<br>Veja um exemplo funcionando em <a href="../appexemplo_v2.0/">Aplicação de exemplo 2.0 > Banco > AutoComplete Multiplos Bancos </a>.'
+.'<br>'
+.'<br>Obserção esse exemplo precisa do SGBD MySQL funcionando e configurado. Com o banco exemplo instalado'
+.'<br>';
+
+$frm = new TForm('Exemplo setAutoComplete Multiplos bancos');
+$frm->setFlat(true);
+$frm->setMaximize(true);
+$frm->addCssFile('css/css_formDinv5.css');
+
+$frm->addHtmlField('html1', $html1, null, null, null, null)->setClass('boxAlert',false);
+
+$frm->show();
