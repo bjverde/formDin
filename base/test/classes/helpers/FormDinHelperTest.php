d0ec7dfd1915187f3e2526218205e3f03dd1fab0
Bjverde <bjverde@yahoo.com.br>
2022-10-16 22:39:52 -0300

    :bug: BASE #277 correção

--- base/test/classes/helpers/FormDinHelperTest.php
+++ base/test/classes/helpers/FormDinHelperTest.php
@@ -50,7 +50,7 @@ use PHPUnit\Framework\TestCase;
 class FormDinHelperTest extends TestCase
 {
     public function testVersion() {
-		$expected = '4.19.0';
+		$expected = '4.20.0';
 		$result =  FormDinHelper::version();
 		$this->assertEquals( $expected , $result);
 	}
