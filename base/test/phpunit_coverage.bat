d7cd7b3558fee9afb01c5e8096ebe7602b9ccf2b
Bjverde <bjverde@yahoo.com.br>
2024-08-29 18:40:29 -0300

    ✅ BASE phpunit correção

--- base/test/phpunit_coverage.bat
+++ base/test/phpunit_coverage.bat
@@ -9,5 +9,5 @@ REM c:\wamp64\bin\php\php7.4.26\php.exe c:\wamp64\bin\phpunit\phpunit-9.1.4.phar
 REM ECHO php 8.0.13 and PHPUnit 9.1.4 with Config XML file
 REM c:\wamp64\bin\php\php8.0.13\php.exe C:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration c:\wamp64\www\formDin\base\test\phpunit-conf-win.xml c:\wamp64\www\formDin\base\test\
 
-ECHO php 8.1.0 and PHPUnit 9.5.9 with Config XML file
-c:\wamp64\bin\php\php8.1.0\php.exe  C:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration C:\wamp64\www\formDin\base\test\phpunit-conf-win.xml C:\wamp64\www\formDin\base\test\
+ECHO php 8.1.28 and PHPUnit 9.5.9 with Config XML file
+d:\wamp64\bin\php\php8.1.28\php.exe  d:\wamp64\bin\phpunit\phpunit-9.5.9.phar --configuration d:\wamp64\www\formDin\base\test\phpunit-conf-win.xml d:\wamp64\www\formDin\base\test\
